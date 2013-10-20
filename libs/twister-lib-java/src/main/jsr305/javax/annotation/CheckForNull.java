cf3693d457c72d44cc15216ef4da44a549dfbd29
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2013-10-20 14:38:56 +0000

    [SVN] r760 Better London Travel in /Libraries/twister-lib-java/ [COM] Some comments for JSR305

--- libs/twister-lib-java/src/main/jsr305/javax/annotation/CheckForNull.java
+++ libs/twister-lib-java/src/main/jsr305/javax/annotation/CheckForNull.java
@@ -4,9 +4,15 @@ import java.lang.annotation.*;
 
 import javax.annotation.meta.*;
 
+/**
+ * The annotated element might be <code>null</code>, and uses of the element should check for <code>null</code>.
+ * When this annotation is applied to a method it applies to the method return value.
+ * @see <a href="http://findbugs.sourceforge.net/manual/annotations.html">FindBugs documentation: Annotations</a>
+ */
 @Documented
 @TypeQualifierNickname
 @Nonnull(when = When.MAYBE)
+//@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CheckForNull {
 	// no parameters
--- libs/twister-lib-java/src/main/jsr305/javax/annotation/Nonnull.java
+++ libs/twister-lib-java/src/main/jsr305/javax/annotation/Nonnull.java
@@ -4,8 +4,15 @@ import java.lang.annotation.*;
 
 import javax.annotation.meta.*;
 
+/**
+ * The annotated element must not be <code>null</code>.
+ * Annotated fields must not be <code>null</code> after construction has completed.
+ * Annotated methods must have non-<code>null</code> return values.
+ * @see <a href="http://findbugs.sourceforge.net/manual/annotations.html">FindBugs documentation: Annotations</a>
+ */
 @Documented
 @TypeQualifier
+// @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Nonnull {
 	When when() default When.ALWAYS;
