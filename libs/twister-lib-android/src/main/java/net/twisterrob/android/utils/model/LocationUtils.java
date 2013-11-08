b2700ff92d5b355f408e0c2799f56d8338b73f66
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2013-11-08 23:54:39 +0000

    [SVN] r791 Better London Travel in /Libraries/twister-lib-android/ [ADD] Station map on a V2 map [ADD] New icons for map, (V2 can't handle shapes)

--- libs/twister-lib-android/src/main/java/net/twisterrob/android/utils/model/LocationUtils.java
+++ libs/twister-lib-android/src/main/java/net/twisterrob/android/utils/model/LocationUtils.java
@@ -2,10 +2,13 @@ package net.twisterrob.android.utils.model;
 
 import net.twisterrob.java.model.Location;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.maps.GeoPoint;
 
 public class LocationUtils extends net.twisterrob.java.model.LocationUtils {
 	private static final double GEO = 1e6d;
+
+	// Maps V1 GeoPoint
 
 	public static GeoPoint toGeoPoint(Location loc) {
 		return new GeoPoint((int)(loc.getLatitude() * GEO), (int)(loc.getLongitude() * GEO));
@@ -22,4 +25,23 @@ public class LocationUtils extends net.twisterrob.java.model.LocationUtils {
 		return Math.abs(geo.getLatitudeE6() / GEO - loc.getLatitude()) < DELTA
 				&& Math.abs(geo.getLongitudeE6() / GEO - loc.getLongitude()) < DELTA;
 	}
+
+	// Maps V2 LatLng
+
+	public static LatLng toLatLng(Location loc) {
+		return new LatLng(loc.getLatitude(), loc.getLongitude());
+	}
+
+	public static Location fromLatLng(LatLng ll) {
+		return new Location(ll.latitude, ll.longitude);
+	}
+
+	public static boolean near(LatLng ll, Location loc) {
+		return near(loc, ll);
+	}
+
+	public static boolean near(Location loc, LatLng ll) {
+		return Math.abs(ll.latitude - loc.getLatitude()) < DELTA && Math.abs(ll.longitude - loc.getLongitude()) < DELTA;
+	}
+
 }
---
+++ libs/twister-lib-android/src/main/java/net/twisterrob/android/utils/tools/DrawableTools.java
@@ -0,0 +1,24 @@
+package net.twisterrob.android.utils.tools;
+
+import android.content.res.Resources;
+import android.graphics.*;
+import android.graphics.drawable.Drawable;
+
+public class DrawableTools {
+	public Bitmap toBitmap(int shapeID, int widthID, int heightID, Resources res) {
+		Drawable shape = res.getDrawable(shapeID);
+		return toBitmap(shape, widthID, heightID, res);
+	}
+	public Bitmap toBitmap(Drawable shape, int widthID, int heightID, Resources res) {
+		int width = res.getDimensionPixelSize(widthID);
+		int height = res.getDimensionPixelSize(heightID);
+		return toBitmap(shape, width, height);
+	}
+	public Bitmap toBitmap(Drawable shape, int width, int height) {
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		shape.setBounds(0, 0, bitmap.getWidth(), bitmap.getHeight());
+		shape.draw(canvas);
+		return bitmap;
+	}
+}
