33b7c506a7f069a84f51c31b90affd124f161946
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2014-11-30 14:16:15 +0000

    [SVN] r1403 Inventory in /Projects/Inventory/ [ADD] Selection capability to all lists

--- src/main/java/net/twisterrob/inventory/android/fragment/data/BaseGalleryFragment.java
+++ src/main/java/net/twisterrob/inventory/android/fragment/data/BaseGalleryFragment.java
@@ -1,11 +1,19 @@
 package net.twisterrob.inventory.android.fragment.data;
 
+import java.util.*;
+
+import org.slf4j.*;
+
 import android.content.Intent;
 import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.*;
 import android.support.v7.widget.GridLayoutManager.SpanSizeLookup;
 import android.support.v7.widget.RecyclerView.*;
-import android.view.View;
+import android.view.*;
 
 import net.twisterrob.android.adapter.CursorRecyclerAdapter;
 import net.twisterrob.inventory.android.R;
@@ -14,8 +22,11 @@ import net.twisterrob.inventory.android.view.*;
 import net.twisterrob.inventory.android.view.GalleryAdapter.GalleryItemEvents;
 
 public abstract class BaseGalleryFragment<T> extends BaseRecyclerFragment<T>
-		implements GalleryItemEvents {
+		implements GalleryItemEvents, ActionMode.Callback {
+	private static final Logger LOG = LoggerFactory.getLogger(BaseGalleryFragment.class);
+
 	private HeaderManager header = null;
+	private SelectionAdapter<? extends ViewHolder> selectionAdapter;
 
 	public void setHeader(BaseFragment headerFragment) {
 		this.header = headerFragment != null? new HeaderManager(this, headerFragment) : null;
@@ -62,8 +73,31 @@ public abstract class BaseGalleryFragment<T> extends BaseRecyclerFragment<T>
 		if (header != null) {
 			adapter = header.wrap(adapter);
 		}
+		adapter = selectionAdapter = new SelectionAdapter<>(adapter);
+		if (header != null) {
+			selectionAdapter.setSelectable(0, false);
+		}
 		list.setAdapter(adapter);
 		return cursorAdapter;
+	}
+
+	@Override public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		if (actionMode != null) {
+			outState.putIntegerArrayList("selection", (ArrayList<Integer>)selectionAdapter.getSelectedItems());
+		}
+	}
+
+	@Override public void onViewStateRestored(Bundle savedInstanceState) {
+		super.onViewStateRestored(savedInstanceState);
+		if (savedInstanceState != null) {
+			ArrayList<Integer> selection = savedInstanceState.getIntegerArrayList("selection");
+			if (selection != null) {
+				tryStartSelectionMode();
+				selectionAdapter.setSelectedItems(selection);
+				updateCount();
+			}
+		}
 	}
 
 	protected abstract boolean canCreateNew();
@@ -71,12 +105,82 @@ public abstract class BaseGalleryFragment<T> extends BaseRecyclerFragment<T>
 	protected abstract void onListItemClick(ViewHolder holder);
 	protected abstract void onListItemLongClick(ViewHolder holder);
 
+	private ActionMode actionMode;
+	protected boolean wantSelection() {
+		return true;
+	}
 	@Override public final void onItemClick(ViewHolder holder) {
-		onListItemClick(holder);
+		if (actionMode != null) {
+			selectionAdapter.toggleSelection(holder.getPosition());
+			updateCount();
+		} else {
+			onListItemClick(holder);
+		}
 	}
 
 	@Override public final boolean onItemLongClick(ViewHolder holder) {
-		onListItemLongClick(holder);
+		if (!tryStartSelectionMode()) {
+			return false;
+		}
+		if (actionMode != null) {
+			selectionAdapter.toggleSelection(holder.getPosition());
+			updateCount();
+		} else {
+			onListItemLongClick(holder);
+		}
 		return true;
+	}
+	private boolean tryStartSelectionMode() {
+		if (actionMode == null && wantSelection()) {
+			FragmentActivity activity = getActivity();
+			if (!(activity instanceof ActionBarActivity)) {
+				LOG.debug("Action Mode is not available if Activity is not an ActionBar one: {}", activity);
+				return false;
+			}
+			actionMode = ((ActionBarActivity)activity).startSupportActionMode(this);
+			return true;
+		}
+		return true;
+	}
+
+	private void updateCount() {
+		actionMode.setTitle(getString(R.string.selection_count, selectionAdapter.getSelectedItemCount()));
+	}
+
+	@Override public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+		mode.getMenuInflater().inflate(R.menu.selection, menu);
+		return true;
+	}
+
+	@Override public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+		return true;
+	}
+
+	@Override public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_select_all:
+				selectionAdapter.selectRange(0, selectionAdapter.getItemCount());
+				updateCount();
+				return true;
+			case R.id.action_select_none:
+				selectionAdapter.clearSelections();
+				updateCount();
+				return true;
+			case R.id.action_select_invert:
+				Set<Integer> selection = new TreeSet<>(selectionAdapter.getSelectedItems());
+				selectionAdapter.clearSelections();
+				for (int i = 0; i < selectionAdapter.getItemCount(); i++) {
+					selectionAdapter.setSelected(i, !selection.contains(i));
+				}
+				updateCount();
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	@Override public void onDestroyActionMode(ActionMode mode) {
+		actionMode = null;
+		selectionAdapter.clearSelections();
 	}
 }
---
+++ src/main/java/net/twisterrob/inventory/android/view/SelectionAdapter.java
@@ -0,0 +1,104 @@
+package net.twisterrob.inventory.android.view;
+
+import java.util.*;
+
+import android.os.Build.*;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.util.SparseBooleanArray;
+
+// TODO override mDataObserver to adjust selectedItems and excludedItems indices when items move
+// based on http://www.grokkingandroid.com/statelistdrawables-for-recyclerview-selection/
+public class SelectionAdapter<VH extends RecyclerView.ViewHolder> extends WrappingAdapter<VH> {
+	private final SparseBooleanArray selectedItems = new SparseBooleanArray();
+	private final SparseBooleanArray excludedItems = new SparseBooleanArray();
+
+	public SelectionAdapter(Adapter<VH> wrapped) {
+		super(wrapped);
+	}
+
+	@Override public void onBindViewHolder(VH holder, int position) {
+		boolean selected = isSelected(position);
+		if (VERSION.SDK_INT < VERSION_CODES.HONEYCOMB) {
+			holder.itemView.setSelected(selected);
+		} else {
+			holder.itemView.setActivated(selected);
+		}
+		super.onBindViewHolder(holder, position);
+	}
+
+	public void setSelectable(int position, boolean isSelectable) {
+		if (!isSelectable) {
+			excludedItems.put(position, true);
+		} else {
+			excludedItems.delete(position);
+			setSelected(position, false);
+		}
+	}
+
+	public boolean isSelectable(int position) {
+		return !excludedItems.get(position);
+	}
+
+	public boolean isSelected(int position) {
+		return selectedItems.get(position);
+	}
+
+	public void setSelected(int position, boolean isSelected) {
+		if (!isSelectable(position)) {
+			return;
+		}
+		if (isSelected) {
+			selectedItems.put(position, true);
+		} else {
+			selectedItems.delete(position);
+		}
+	}
+
+	public void toggleSelection(int position) {
+		if (!isSelectable(position)) {
+			return;
+		}
+		setSelected(position, !isSelected(position));
+		notifyItemChanged(position);
+	}
+
+	public void selectRange(int positionStart, int itemCount) {
+		selectedItems.clear();
+		for (int position = positionStart; position < positionStart + itemCount; position++) {
+			if (isSelectable(position)) {
+				selectedItems.append(position, true);
+			}
+		}
+		notifyItemRangeChanged(positionStart, itemCount);
+	}
+
+	public void clearSelections() {
+		selectedItems.clear();
+		notifyDataSetChanged();
+	}
+
+	public int getSelectedItemCount() {
+		return selectedItems.size();
+	}
+
+	public List<Integer> getSelectedItems() {
+		List<Integer> items = new ArrayList<>(selectedItems.size());
+		for (int i = 0; i < selectedItems.size(); i++) {
+			int key = selectedItems.keyAt(i);
+			assert selectedItems.get(key) : "selected items contains a 'false' element";
+			items.add(key);
+		}
+		return items;
+	}
+
+	public void setSelectedItems(Collection<Integer> positions) {
+		selectedItems.clear();
+		for (int position : positions) {
+			if (isSelectable(position)) {
+				selectedItems.put(position, true);
+			}
+		}
+		notifyDataSetChanged();
+	}
+}
