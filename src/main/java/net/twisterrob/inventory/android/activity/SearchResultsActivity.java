cc617713a4d372cebd0d351ba8d1fe4c3df4a319
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2014-12-19 11:36:53 +0000

    [SVN] r1452 Inventory in /Projects/Inventory/ [REM] Search activity re-use

--- src/main/java/net/twisterrob/inventory/android/activity/SearchResultsActivity.java
+++ src/main/java/net/twisterrob/inventory/android/activity/SearchResultsActivity.java
@@ -6,54 +6,30 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager.OnBackStackChangedListener;
 import android.support.v4.app.TaskStackBuilder;
-import android.view.Menu;
 
-import net.twisterrob.android.utils.tools.AndroidTools;
 import net.twisterrob.inventory.android.*;
 import net.twisterrob.inventory.android.activity.data.*;
 import net.twisterrob.inventory.android.fragment.data.ItemListFragment;
 import net.twisterrob.inventory.android.fragment.data.ItemListFragment.ItemsEvents;
 
-public class SearchResultsActivity extends BaseDetailActivity<ItemListFragment>
-		implements ItemsEvents, OnBackStackChangedListener {
+public class SearchResultsActivity extends BaseDetailActivity<ItemListFragment> implements ItemsEvents {
 	private static final Logger LOG = LoggerFactory.getLogger(SearchResultsActivity.class);
 
-	@Override protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getSupportFragmentManager().addOnBackStackChangedListener(this);
-	}
 	@Override
 	protected ItemListFragment onCreateFragment(Bundle savedInstanceState) {
-		setActionBarTitle(getTitle());
-		LOG.trace("onCreate({})", getIntent());
 		return handleIntent();
 	}
 
-	@Override public void onBackStackChanged() {
-		ItemListFragment fragment = getFragment();
-		if (fragment != null) {
-			String query = fragment.getArguments().getString(SearchManager.QUERY);
-			setActionBarSubtitle(query);
-		}
-	}
-
-	@Override protected void onResume() {
-		super.onResume();
-		onBackStackChanged();
-	}
 	@Override
 	protected void onNewIntent(Intent intent) {
-		LOG.trace("onNewIntent({}->{})", getIntent(), intent);
+		LOG.trace("onNewIntent(\n{} {}\n->\n{} {}\n)",
+				getIntent(), getIntent().getExtras(), intent, intent.getExtras());
 		setIntent(intent);
 		super.onNewIntent(intent);
 		ItemListFragment fragment = handleIntent();
 		if (fragment != null) {
-			updateFragment(fragment)
-					.addToBackStack(null)
-					.commit()
-			;
+			updateFragment(fragment).commit();
 		}
 	}
 
@@ -62,6 +38,8 @@ public class SearchResultsActivity extends BaseDetailActivity<ItemListFragment>
 		CharSequence query = getExtraQuery(intent);
 		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
 			LOG.debug("Search '{}'", query);
+			setActionBarTitle(getTitle());
+			setActionBarSubtitle(query);
 			return ItemListFragment.newSearchInstance(query);
 		} else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
 			Uri uri = fixUri(intent.getData());
@@ -94,22 +72,12 @@ public class SearchResultsActivity extends BaseDetailActivity<ItemListFragment>
 				;
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-
-		getMenuInflater().inflate(R.menu.search, menu);
-		AndroidTools.prepareSearch(this, menu, R.id.search);
-
-		return true;
-	}
-
 	public void itemSelected(long itemID) {
 		startActivity(ItemViewActivity.show(itemID));
 	}
 
 	public void itemActioned(long itemID) {
-		startActivity(ItemEditActivity.edit(itemID));
+		itemSelected(itemID);
 	}
 
 	public void newItem(long parentID) {
