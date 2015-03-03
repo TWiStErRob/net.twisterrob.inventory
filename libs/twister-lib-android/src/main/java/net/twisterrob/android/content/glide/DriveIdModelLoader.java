1615400296394a458c10a910eb933ec9d248539a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2015-03-03 11:57:52 +0000

    [SVN] r1465 twister-lib-android in /Libraries/twister-lib-android/ [ADD] Drive ModelLoader.Factory (http://stackoverflow.com/questions/25148291)

--- libs/twister-lib-android/src/main/java/net/twisterrob/android/content/glide/DriveIdModelLoader.java
+++ libs/twister-lib-android/src/main/java/net/twisterrob/android/content/glide/DriveIdModelLoader.java
@@ -2,7 +2,10 @@ package net.twisterrob.android.content.glide;
 
 import java.io.InputStream;
 
+import android.content.Context;
+
 import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.model.*;
 import com.bumptech.glide.load.model.stream.StreamModelLoader;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.drive.DriveId;
@@ -16,5 +19,21 @@ public class DriveIdModelLoader implements StreamModelLoader<DriveId> {
 
 	public DataFetcher<InputStream> getResourceFetcher(DriveId model, int width, int height) {
 		return new DriveIdDataFetcher(client, model);
+	}
+
+	public static class Factory implements ModelLoaderFactory<DriveId, InputStream> {
+		private final GoogleApiClient client;
+
+		public Factory(GoogleApiClient client) {
+			this.client = client;
+		}
+
+		public ModelLoader<DriveId, InputStream> build(Context context, GenericLoaderFactory factories) {
+			return new DriveIdModelLoader(client);
+		}
+
+		public void teardown() {
+			client.disconnect();
+		}
 	}
 }
