736f022a2fd0a2701fe530b04c34d1904cacbfb6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2015-04-12 22:25:15 +0000

    [SVN] r1525 Inventory in /Projects/Inventory/ [ADD] Missing svg icons for main categories

---
+++ src/main/res/raw/helpers.js
@@ -0,0 +1,69 @@
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+	var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+	return {
+		x: centerX + (radius * Math.cos(angleInRadians)),
+		y: centerY + (radius * Math.sin(angleInRadians))
+	};
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+	var start = polarToCartesian(x, y, radius, endAngle);
+	var end = polarToCartesian(x, y, radius, startAngle);
+	var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+	return [
+			"M", x, y,
+			"l", start.x, start.y,
+			"A", radius, radius, 0, arcSweep, 0, end.x, end.y,
+			"Z"
+	].join(" ");
+	return d;
+}
+
+function rounded_rect_uni(x, y, w, h, tl, tr, bl, br) {
+	return rounded_rect(x, y, w, h, tl, tl, tr, tr, bl, bl, br, br);
+}
+
+function rounded_rect(x, y, w, h, rTLx, rTLy, rTRx, rTRy, rBLx, rBLy, rBRx, rBRy) {
+	console.debug(arguments);
+	var path = "";
+	path += "M" + (x + rTLx) + "," + y;
+	path += "\n";
+	path += "h" + (w - (rTLx + rTRx));
+	path += "\n";
+	if (rTRx > 0 && rTRy > 0) {
+		path += "a " + rTRx + "," + rTRy + " 0 0 1 " + rTRx + "," + rTRy;
+	} else {
+		path += "h" + rTRx;
+		path += "v" + rTRy;
+	}
+	path += "\n";
+	path += "v" + (h - (rTRy + rBRy));
+	path += "\n";
+	if (rBRx > 0 && rBRy > 0) {
+		path += "a " + rBRx + "," + rBRy + " 0 0 1 " + -rBRx + "," + rBRy;
+	} else {
+		path += "v" + rBRy;
+		path += "h" + -rBRx;
+	}
+	path += "\n";
+	path += "h" + -(w - (rBRx + rBLx));
+	path += "\n";
+	if (rBLx > 0 && rBLy > 0) {
+		path += "a " + rBLx + "," + rBLy + " 0 0 1 " + -rBLx + "," + -rBLy;
+	} else {
+		path += "h" + -rBLx;
+		path += "v" + -rBLy;
+	}
+	path += "\n";
+	path += "v" + -(h - (rBLy + rTLy));
+	path += "\n";
+	if (rTLx > 0 && rTLy > 0) {
+		path +=  "a " + rTLx + "," + rTLy + " 0 0 1 " + rTLx + "," + -rTLy;
+	} else {
+		path += "v" + -rTLy
+		path += "h" + rTLx;
+	}
+	path += "\n";
+	path += "z";
+	return path;
+}
