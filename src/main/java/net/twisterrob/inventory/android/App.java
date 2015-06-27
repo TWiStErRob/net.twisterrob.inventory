6d9a91e19f31a0c74918538ed0710196c0d5ecd1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2015-06-27 12:21:17 +0000

    [SVN] r1646 Inventory in /Libraries/twister-lib-android/ [FIX] Update gradle to latest release [MOD] Enable parallel and on-demand in gradle [FIX] Don't crash if DB is dead to be able to access Manage Space

--- src/main/java/net/twisterrob/inventory/android/App.java
+++ src/main/java/net/twisterrob/inventory/android/App.java
@@ -87,9 +87,13 @@ public class App extends Application {
 			App.toast(message);
 			new BackgroundExecution(new Runnable() {
 				@Override public void run() {
-					database.updateCategoryCache(App.getAppContext());
-					LOG.debug("Locale update successful: {} -> {}", storedLanguage, currentLanguage);
-					prefs.edit().putString(Prefs.CURRENT_LANGUAGE, currentLanguage).apply();
+					try {
+						database.updateCategoryCache(App.getAppContext());
+						LOG.debug("Locale update successful: {} -> {}", storedLanguage, currentLanguage);
+						prefs.edit().putString(Prefs.CURRENT_LANGUAGE, currentLanguage).apply();
+					} catch (Exception ex) {
+						LOG.error("Locale update failed: {} -> {}", storedLanguage, currentLanguage, ex);
+					}
 				}
 			}).execute();
 		}
