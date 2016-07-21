ec47c03182d27553e9107bc598116347aaa482a5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2016-07-21 23:18:02 +0000

    [SVN] r1969 Better London Travel in /Libraries/twister-lib-android/ [REF] Extract a fragment for nearest station in distance map [EXP] Options right drawer for distance options (non-functional) [FIX] FAB opens options drawer [FIX] Show bottom sheet when marker is tapped [FIX] Hide zoom buttons under FAB [FIX] Async Geocoder loop on error

---
+++ libs/twister-lib-android/src/main/java/net/twisterrob/android/adapter/StaticFragmentPagerAdapter.java
@@ -0,0 +1,31 @@
+package net.twisterrob.android.adapter;
+
+import java.util.*;
+
+import android.support.v4.app.*;
+
+public class StaticFragmentPagerAdapter extends FragmentPagerAdapter {
+	private final List<Fragment> fragments = new ArrayList<>();
+	private final List<String> fragmentTitles = new ArrayList<>();
+	public StaticFragmentPagerAdapter(FragmentManager manager) {
+		super(manager);
+	}
+
+	public <T extends Fragment> T add(String title, T fragment) {
+		fragments.add(fragment);
+		fragmentTitles.add(title);
+		return fragment;
+	}
+
+	@Override public Fragment getItem(int position) {
+		return fragments.get(position);
+	}
+
+	@Override public int getCount() {
+		return fragments.size();
+	}
+
+	@Override public CharSequence getPageTitle(int position) {
+		return fragmentTitles.get(position);
+	}
+}
