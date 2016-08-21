24354b5eca94d52db648dab13cbadc6ff2eb8115
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2016-08-21 15:46:30 +0000

    [SVN] r2083 Inventory in /Projects/Inventory/ [FIX] Be more specific with JUnit4 defaults

--- src/test/java/net/twisterrob/test/frameworks/JUnit4Test.java
+++ src/test/java/net/twisterrob/test/frameworks/JUnit4Test.java
@@ -2,10 +2,13 @@ package net.twisterrob.test.frameworks;
 
 import org.junit.*;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
-public class JUnitTest {
+@RunWith(JUnit4.class)
+public class JUnit4Test {
 	@Rule public ExpectedException thrown = ExpectedException.none();
 
 	@Test public void testRunner() {
