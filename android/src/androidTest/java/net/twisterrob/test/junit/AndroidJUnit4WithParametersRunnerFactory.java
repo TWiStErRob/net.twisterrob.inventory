6e11ac6756ea28daf8d4831fa93e46f82cc09dd0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2016-10-01 16:55:22 +0000

    [SVN] r2216 Inventory in /Projects/Inventory/ [ADD] Support for parameterized Android tests

---
+++ android/src/androidTest/java/net/twisterrob/test/junit/AndroidJUnit4WithParametersRunnerFactory.java
@@ -0,0 +1,30 @@
+package net.twisterrob.test.junit;
+
+import org.junit.runner.Runner;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.parameterized.*;
+
+import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.internal.runner.RunnerArgs;
+import android.support.test.internal.util.AndroidRunnerParams;
+
+import net.twisterrob.java.utils.ReflectionTools;
+
+/**
+ * @see AndroidJUnit4WithParameters
+ */
+public class AndroidJUnit4WithParametersRunnerFactory implements ParametersRunnerFactory {
+	public Runner createRunnerForTestWithParameters(TestWithParameters test)
+			throws InitializationError {
+		Instrumentation instr = InstrumentationRegistry.getInstrumentation();
+		RunnerArgs runnerArgs = ReflectionTools.get(instr, "mRunnerArgs");
+		AndroidRunnerParams runnerParams = new AndroidRunnerParams(
+				instr,
+				InstrumentationRegistry.getArguments(),
+				runnerArgs != null && runnerArgs.logOnly,
+				runnerArgs != null? runnerArgs.testTimeout : 0,
+				true);
+		return new AndroidJUnit4WithParameters(test, runnerParams);
+	}
+}
