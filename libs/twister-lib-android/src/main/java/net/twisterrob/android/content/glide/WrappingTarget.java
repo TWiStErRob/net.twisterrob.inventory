767ea7a39ee4404c9a708aa7a0d55241a54ca3e0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2017-03-12 17:17:42 +0000

    [SVN] r2286 twister-lib-android in /Libraries/twister-lib-android/ [REF] Rename field to better reflect intent

--- libs/twister-lib-android/src/main/java/net/twisterrob/android/content/glide/WrappingTarget.java
+++ libs/twister-lib-android/src/main/java/net/twisterrob/android/content/glide/WrappingTarget.java
@@ -8,55 +8,55 @@ import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.*;
 
 public class WrappingTarget<Z> implements Target<Z> {
-	@NonNull protected final Target<? super Z> target;
-	public WrappingTarget(@NonNull Target<? super Z> target) {
-		this.target = target;
+	@NonNull protected final Target<? super Z> wrapped;
+	public WrappingTarget(@NonNull Target<? super Z> wrapped) {
+		this.wrapped = wrapped;
 	}
 
 	@Override public void getSize(SizeReadyCallback cb) {
-		target.getSize(cb);
+		wrapped.getSize(cb);
 	}
 
 	@Override public void onLoadStarted(Drawable placeholder) {
-		target.onLoadStarted(placeholder);
+		wrapped.onLoadStarted(placeholder);
 	}
 	@Override public void onLoadFailed(Exception e, Drawable errorDrawable) {
-		target.onLoadFailed(e, errorDrawable);
+		wrapped.onLoadFailed(e, errorDrawable);
 	}
 	@SuppressWarnings({"rawtypes", "unchecked"})
 	@Override public void onResourceReady(Z resource, GlideAnimation<? super Z> glideAnimation) {
-		target.onResourceReady(resource, (GlideAnimation)glideAnimation);
+		wrapped.onResourceReady(resource, (GlideAnimation)glideAnimation);
 	}
 	@Override public void onLoadCleared(Drawable placeholder) {
-		target.onLoadCleared(placeholder);
+		wrapped.onLoadCleared(placeholder);
 	}
 
 	@Override public Request getRequest() {
-		return target.getRequest();
+		return wrapped.getRequest();
 	}
 	@Override public void setRequest(Request request) {
-		target.setRequest(request);
+		wrapped.setRequest(request);
 	}
 
 	@Override public void onStart() {
-		target.onStart();
+		wrapped.onStart();
 	}
 	@Override public void onStop() {
-		target.onStop();
+		wrapped.onStop();
 	}
 	@Override public void onDestroy() {
-		target.onDestroy();
+		wrapped.onDestroy();
 	}
 	@Override public int hashCode() {
-		return target.hashCode();
+		return wrapped.hashCode();
 	}
 	@Override public boolean equals(Object o) {
 		if (o instanceof WrappingTarget) {
-			return this.target.equals(((WrappingTarget<?>)o).target);
+			return this.wrapped.equals(((WrappingTarget<?>)o).wrapped);
 		}
 		return super.equals(o);
 	}
 	@Override public String toString() {
-		return "Wrapped " + target.toString();
+		return "Wrapped " + wrapped.toString();
 	}
 }
