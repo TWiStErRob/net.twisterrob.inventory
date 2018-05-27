fdb52cf2440269efb3a1fc507c06eded9f11d11e
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-05-27 15:30:11 +0000

    [SVN] r2508 Inventory in /Libraries/twister-lib-android/ [EXP] Modularize twister-lib-* [MOD] Upgrade Gradle wrappers [FIX] Clean up Gradle files

--- libs/twister-lib-java/lib/java/src/test/java/net/twisterrob/java/utils/StringToolsTest.java
+++ libs/twister-lib-java/lib/java/src/test/java/net/twisterrob/java/utils/StringToolsTest.java
@@ -3,9 +3,11 @@ package net.twisterrob.java.utils;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class StringToolsTest {
+
 	private static final String EMPTY = "";
 	private static final String TEXT = "abcdeb";
 	private static final String MATCH = "b";
@@ -17,14 +19,17 @@ public class StringToolsTest {
 		assertEquals(EMPTY.endsWith(MATCH), StringTools.endsWith(new StringBuilder(EMPTY), MATCH));
 		assertEquals(EMPTY.endsWith(MATCH_LONG), StringTools.endsWith(new StringBuilder(EMPTY), MATCH_LONG));
 	}
+
 	@Test public void endsWithEmptySearch() {
 		assertEquals(EMPTY.endsWith(EMPTY), StringTools.endsWith(new StringBuilder(EMPTY), EMPTY));
 		assertEquals(TEXT.endsWith(EMPTY), StringTools.endsWith(new StringBuilder(TEXT), EMPTY));
 	}
+
 	@Test public void endsWithMatch() {
 		assertEquals(TEXT.endsWith(MATCH), StringTools.endsWith(new StringBuilder(TEXT), MATCH));
 		assertEquals(TEXT.endsWith(MATCH_LONG), StringTools.endsWith(new StringBuilder(TEXT), MATCH_LONG));
 	}
+
 	@Test public void endsWithMisMatch() {
 		assertEquals(TEXT.endsWith(MISMATCH), StringTools.endsWith(new StringBuilder(TEXT), MISMATCH));
 		assertEquals(TEXT.endsWith(MISMATCH_LONG), StringTools.endsWith(new StringBuilder(TEXT), MISMATCH_LONG));
@@ -36,6 +41,7 @@ public class StringToolsTest {
 		assertThat(obj.toString(), containsString(hash));
 		assertThat(obj.toString(), matchesPattern("^java\\.lang\\.Object@" + hash + "$"));
 	}
+
 	@Test public void hashStringNull() {
 		assertEquals("0", StringTools.hashString(null));
 	}
