6311869523f12a7d813d42ee251c63af9e42bfc5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-05-27 17:51:26 +0000

    [SVN] r2511 Inventory in /Libraries/twister-lib-java/ [REF] Move a few more hamcrest matchers to module [REF] Move diff utilities to module

--- libs/twister-lib-java/lib/hamcrest/src/main/java/net/twisterrob/test/hamcrest/Matchers.java
+++ libs/twister-lib-java/lib/hamcrest/src/main/java/net/twisterrob/test/hamcrest/Matchers.java
@@ -1,12 +1,38 @@
 package net.twisterrob.test.hamcrest;
 
+import java.util.Arrays;
+import java.util.zip.ZipEntry;
+
 import javax.annotation.*;
 
-import org.hamcrest.Matcher;
+import org.hamcrest.*;
 
 import static org.hamcrest.Matchers.*;
 
 public class Matchers {
+	public static @Nonnull Matcher<Class<?>> hasConstant(String constantName, Matcher<?> valueMatcher) {
+		return new ConstantMatcher(constantName, valueMatcher);
+	}
+
+	public static <T> ExactlyOneOf<T> exactlyOneOf(Iterable<Matcher<? super T>> matchers) {
+		return new ExactlyOneOf<>(matchers);
+	}
+
+	@SafeVarargs
+	public static <T> ExactlyOneOf<T> exactlyOneOf(Matcher<? super T>... matchers) {
+		return exactlyOneOf(Arrays.asList(matchers));
+	}
+
+	public static Matcher<ZipEntry> zipEntryWithName(String name) {
+		return zipEntryWithName(is(name));
+	}
+	public static Matcher<ZipEntry> zipEntryWithName(Matcher<String> nameMatchers) {
+		return new FeatureMatcher<ZipEntry, String>(nameMatchers, "zip entry with name", "entry name") {
+			@Override protected String featureValueOf(ZipEntry actual) {
+				return actual.getName();
+			}
+		};
+	}
 
 	public static <T extends Throwable> Matcher<T> hasCause(final @Nullable Throwable cause) {
 		return hasCause(sameInstance(cause));
