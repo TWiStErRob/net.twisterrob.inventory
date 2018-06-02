6ba5f05ca35aeccb2ea11b87f2188caf5ef461fe
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-06-02 22:35:20 +0000

    [SVN] r2525 Inventory in /Libraries/twister-lib-android/ [FIX] Delay callback removal from ActivityMonitor, the callbacks are called in a loop

--- libs/twister-lib-android/espresso/src/main/java/net/twisterrob/android/test/espresso/idle/AllActivitiesDestroyedIdlingResource.java
+++ libs/twister-lib-android/espresso/src/main/java/net/twisterrob/android/test/espresso/idle/AllActivitiesDestroyedIdlingResource.java
@@ -3,6 +3,7 @@ package net.twisterrob.android.test.espresso.idle;
 import java.util.*;
 
 import android.app.Activity;
+import android.os.*;
 import android.support.annotation.AnyThread;
 import android.support.test.runner.lifecycle.*;
 
@@ -15,7 +16,14 @@ public class AllActivitiesDestroyedIdlingResource extends AsyncIdlingResource {
 			if (stage == Stage.DESTROYED) {
 				activities.remove(activity);
 				if (isIdleCore()) {
-					monitor.removeLifecycleCallback(callback);
+					// remove our listener in the next loop, otherwise we get a "Recursive looping detected!"
+					// from android.support.test.espresso.base.UiControllerImpl.loopUntil
+					// or a ConcurrentModificationException in the loop.
+					new Handler(Looper.getMainLooper()).post(new Runnable() {
+						@Override public void run() {
+							monitor.removeLifecycleCallback(callback);
+						}
+					});
 					transitionToIdle();
 				}
 			}
