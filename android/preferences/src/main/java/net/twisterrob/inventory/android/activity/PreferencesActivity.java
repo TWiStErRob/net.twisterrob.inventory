4f6f382fc9b0a8bbeb6bc703f27e0db79932f70e
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-04-25 10:52:01 +0000

    [SVN] r3141 Inventory in /Projects/Inventory/ [FIX] Ignore unavoidable StrictMode disk read/write in Settings

--- android/preferences/src/main/java/net/twisterrob/inventory/android/activity/PreferencesActivity.java
+++ android/preferences/src/main/java/net/twisterrob/inventory/android/activity/PreferencesActivity.java
@@ -5,7 +5,8 @@ import java.util.List;
 import android.content.*;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.ResolveInfo;
-import android.os.Bundle;
+import android.os.*;
+import android.os.StrictMode.ThreadPolicy;
 import android.preference.*;
 import android.support.annotation.NonNull;
 
@@ -17,7 +18,37 @@ public class PreferencesActivity extends PreferenceActivity
 	@Override public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		addPreferencesFromResource(R.xml.preferences);
+		ThreadPolicy threadPolicy = StrictMode.allowThreadDiskWrites();
+		try {
+			// D/StrictMode: StrictMode policy violation; ~duration=73 ms: android.os.strictmode.DiskWriteViolation
+			// at android.system.Os.mkdir(Os.java:375)
+			// at android.app.ContextImpl.ensurePrivateDirExists(ContextImpl.java:648)
+			// at android.app.ContextImpl.ensurePrivateDirExists(ContextImpl.java:636)
+			// at android.app.ContextImpl.getPreferencesDir(ContextImpl.java:592)
+
+			// D/StrictMode: StrictMode policy violation; ~duration=9 ms: android.os.strictmode.DiskReadViolation
+			// at java.io.File.exists(File.java:815)
+			// at android.app.ContextImpl.getDataDir(ContextImpl.java:2340)
+			// at android.app.ContextImpl.getPreferencesDir(ContextImpl.java:590)
+
+			// at android.app.ContextImpl.getPreferencesDir(continued from above 2)
+			// at android.app.ContextImpl.getSharedPreferencesPath(ContextImpl.java:787)
+			// at android.app.ContextImpl.getSharedPreferences(ContextImpl.java:439)
+			// at android.content.ContextWrapper.getSharedPreferences(ContextWrapper.java:178)
+			// at android.preference.PreferenceManager.getSharedPreferences(PreferenceManager.java:529)
+			// at android.preference.Preference.getSharedPreferences(Preference.java:1233)
+			// at android.preference.Preference.dispatchSetInitialValue(Preference.java:1586)
+			// at android.preference.Preference.onAttachedToHierarchy(Preference.java:1358)
+			// at android.preference.PreferenceInflater.onMergeRoots(PreferenceInflater.java:117)
+			// at android.preference.PreferenceInflater.onMergeRoots(PreferenceInflater.java:44)
+			// at android.preference.GenericInflater.inflate(GenericInflater.java:328)
+			// at android.preference.GenericInflater.inflate(GenericInflater.java:271)
+			// at android.preference.PreferenceManager.inflateFromResource(PreferenceManager.java:343)
+			// at android.preference.PreferenceActivity.addPreferencesFromResource(PreferenceActivity.java:1541)
+			addPreferencesFromResource(R.xml.preferences);
+		} finally {
+			StrictMode.setThreadPolicy(threadPolicy);
+		}
 	}
 
 	@Override protected void onStart() {
