b44f30225b9e21815f4489a0e07d14173fbe5b18
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-11-01 22:31:51 +0000

    [SVN] r3200 Inventory in /Projects/Inventory/ [MOD] TWiStErRob/net.twisterrob.inventory#178 minSdkVersion = 21 [REM] MultiDex, ART supports it by default

--- android/src/debug/java/net/twisterrob/inventory/android/DebugApp.java
+++ android/src/debug/java/net/twisterrob/inventory/android/DebugApp.java
@@ -1,29 +1,5 @@
 package net.twisterrob.inventory.android;
 
-import android.content.Context;
-import android.os.StrictMode;
-import android.os.StrictMode.ThreadPolicy;
-import android.support.multidex.MultiDex;
-
 public class DebugApp extends App {
-
-	@Override
-	protected void attachBaseContext(Context base) {
-		super.attachBaseContext(base);
-		installMultiDex();
-	}
-
-	/**
-	 * Enable MultiDEX only for debug builds, the production build should shrink below the limit for now.
-	 */
-	private void installMultiDex() {
-		ThreadPolicy originalPolicy = StrictMode.allowThreadDiskWrites();
-		try {
-			// StrictModeDiskReadViolation: MultiDexExtractor.load -> getZipCrc -> ZipUtil.getZipCrc
-			// StrictModeDiskWriteViolation: MultiDexExtractor.load -> lockFile
-			MultiDex.install(this);
-		} finally {
-			StrictMode.setThreadPolicy(originalPolicy);
-		}
-	}
+	// MultiDex was here, debug-only overrides come here.
 }
