6612bc07d6b0475d1b9f7909f646270354088ac3
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-05 21:03:21 +0000

    Move android setup (allprojects) to convention plugins

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/android.base.gradle.kts
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/android.base.gradle.kts
@@ -4,4 +4,13 @@ import net.twisterrob.inventory.build.dsl.android
 
 @Suppress("UnstableApiUsage")
 android {
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_7
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	lintOptions {
+		isCheckReleaseBuilds = false
+		baselineFile = rootDir.resolve("config/lint/lint-baseline-${project.name}.xml")
+		lintConfig = rootDir.resolve("config/lint/lint.xml")
+	}
 }
