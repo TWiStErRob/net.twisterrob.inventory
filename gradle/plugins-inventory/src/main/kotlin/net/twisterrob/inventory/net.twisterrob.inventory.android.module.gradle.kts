9b88ec59bb30da5c4fdd65c0536fae0c5f2d4e2b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-05 21:01:11 +0000

    Move a block for disabling androidTest

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.android.module.gradle.kts
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.android.module.gradle.kts
@@ -2,3 +2,16 @@ plugins {
 	id("net.twisterrob.android-library")
 	id("net.twisterrob.inventory.build.android.base")
 }
+
+afterEvaluate {
+	@Suppress("DEPRECATION") // REPORT cannot replace with new interface, missing methods
+	val androidTest = project.android.sourceSets["androidTest"] as com.android.build.gradle.api.AndroidSourceSet
+	if (androidTest.java.getSourceFiles().isEmpty) {
+		logger.info("Disabling AndroidTest tasks in ${project.path} as it has no sources in ${androidTest.java.srcDirs}")
+		tasks.whenTaskAdded {
+			if (this.name.contains("AndroidTest")) {
+				this.enabled = false
+			}
+		}
+	}
+}
