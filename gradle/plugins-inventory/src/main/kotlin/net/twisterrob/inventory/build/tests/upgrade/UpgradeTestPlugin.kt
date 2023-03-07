9ba4612b1b299f89892811a4f06a7482c00bfc22
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-07 12:49:52 +0000

    Migrate UpgradeTestPlugin to the new variant API

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/tests/upgrade/UpgradeTestPlugin.kt
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/tests/upgrade/UpgradeTestPlugin.kt
@@ -1,19 +1,34 @@
 package net.twisterrob.inventory.build.tests.upgrade
 
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
+import com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask
+import net.twisterrob.gradle.android.androidComponents
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.register
 
 class UpgradeTestPlugin : Plugin<Project> {
 
 	override fun apply(project: Project) {
-		val upgradeTest = project.tasks.register<UpgradeTestTask>("upgradeTest") {
-			enabled = false // TODO not working since AGP 3.3/3.4
-			dependsOn("assembleDebug")
-			dependsOn("assembleAndroidTest")
-		}
-		project.tasks.named("connectedCheck").configure {
-			dependsOn(upgradeTest)
+		(project.androidComponents as ApplicationAndroidComponentsExtension).onVariants { variant ->
+			if (variant.androidTest == null) return@onVariants
+			val upgradeTest = project.tasks.register<UpgradeTestTask>("upgradeTest") {
+				enabled = false // TODO not working since AGP 3.3/3.4
+				testedVariant.set(variant)
+				val connectedAndroidTest = project.tasks.named("connectedAndroidTest")
+
+				@Suppress("UNCHECKED_CAST")
+				val instrument = connectedAndroidTest.get().dependsOn.single()
+					as TaskProvider<DeviceProviderInstrumentTestTask>
+				instrumentTestTask.set(instrument)
+
+				dependsOn("assembleDebug")
+				dependsOn("assembleAndroidTest")
+			}
+			project.tasks.named("connectedCheck").configure {
+				dependsOn(upgradeTest)
+			}
 		}
 	}
 }
--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/tests/upgrade/UpgradeTestTask.kt
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/tests/upgrade/UpgradeTestTask.kt
@@ -1,15 +1,16 @@
 package net.twisterrob.inventory.build.tests.upgrade
 
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.api.ApplicationVariant
-import com.android.build.gradle.internal.api.ApplicationVariantImpl
+import com.android.build.api.artifact.Artifacts
+import com.android.build.api.artifact.SingleArtifact
+import com.android.build.api.component.impl.ComponentImpl
+import com.android.build.api.variant.ApplicationVariant
+import com.android.build.api.variant.Variant
 import com.android.build.gradle.internal.services.VariantServices
 import com.android.build.gradle.internal.tasks.DeviceProviderInstrumentTestTask
 import com.android.build.gradle.internal.test.report.ReportType
 import com.android.build.gradle.internal.test.report.ResilientTestReport
 import com.android.build.gradle.internal.testing.ConnectedDevice
 import com.android.build.gradle.internal.testing.TestData
-import com.android.build.gradle.internal.variant.BaseVariantData
 import com.android.builder.testing.api.DeviceConnector
 import com.android.ddmlib.IDevice
 import com.android.ddmlib.testrunner.RemoteAndroidTestRunner
@@ -18,6 +19,7 @@ import com.android.utils.StdLogger
 import net.twisterrob.gradle.android.androidComponents
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.TaskAction
 import org.gradle.work.DisableCachingByDefault
 import java.io.File
@@ -27,17 +29,15 @@ import java.util.concurrent.TimeUnit
 @DisableCachingByDefault(because = "Lots of external factors")
 abstract class UpgradeTestTask : DefaultTask() {
 
+	abstract val testedVariant: Property<ApplicationVariant>
+
+	abstract val instrumentTestTask: Property<DeviceProviderInstrumentTestTask>
+
 	@Suppress("LongMethod") // Will be split up when I make it work again.
 	@TaskAction
 	fun upgradeTest() {
-		val android = project.extensions.findByName("android") as AppExtension
-		val debugVariant = android
-			.applicationVariants
-			.single { it.buildType.name == "debug" }
-			as ApplicationVariantImpl
-
-		val instrument = debugVariant.testVariant.connectedInstrumentTestProvider.get() as
-			DeviceProviderInstrumentTestTask
+		val debugVariant = testedVariant.get()
+		val instrument = instrumentTestTask.get()
 		val adb = project.androidComponents.sdkComponents.adb
 		val deviceProvider = instrument.deviceProviderFactory.getDeviceProvider(adb, null)
 		val device = try {
@@ -48,13 +48,14 @@ abstract class UpgradeTestTask : DefaultTask() {
 		}
 		val realDevice = device.iDevice
 
-		val testApk = debugVariant.testVariant.outputs.first().outputFile
-		logger.info("Uninstalling test package: ${debugVariant.testVariant.applicationId}")
-		realDevice.uninstallPackage(debugVariant.testVariant.applicationId)
+		val testApk = debugVariant.androidTest!!.artifacts.apk
+		val testApplicationId = debugVariant.androidTest!!.applicationId.get()
+		logger.info("Uninstalling test package: ${testApplicationId}")
+		realDevice.uninstallPackage(testApplicationId)
 		logger.info("Installing test package: ${testApk}")
 		realDevice.installPackage(testApk.absolutePath, false)
 
-		val services = debugVariant.variantData.services
+		val services = debugVariant.services
 
 		val results = services.projectInfo.getTestResultsFolder()!!.resolve("upgrade-tests")
 			.also { FileUtils.cleanOutputDir(it) }
@@ -76,7 +77,7 @@ abstract class UpgradeTestTask : DefaultTask() {
 				instrument.testData.get(), device, testListener, reports.resolve("index.html"),
 				"net.twisterrob.inventory.android.UpgradeTests#testPrepareVersion1"
 			)
-			val newApk = debugVariant.outputs.first().outputFile
+			val newApk = debugVariant.artifacts.apk
 			logger.info("Installing package: ${newApk}")
 			realDevice.installPackage(newApk.absolutePath, true)
 			runTest(
@@ -98,18 +99,20 @@ abstract class UpgradeTestTask : DefaultTask() {
 	}
 
 	private fun installOld(realDevice: IDevice, debugVariant: ApplicationVariant, version: String) {
+		val applicationId = debugVariant.applicationId.get()
 		// FIXME release debug build as well
 		val oldApk =
-			File("${System.getenv("RELEASE_HOME")}/android/${debugVariant.applicationId}@${version}d+debug.apk")
-		logger.info("Uninstalling package: ${debugVariant.applicationId}")
-		realDevice.uninstallPackage(debugVariant.applicationId)
+			File("${System.getenv("RELEASE_HOME")}/android/${applicationId}@${version}d+debug.apk")
+		logger.info("Uninstalling package: ${applicationId}")
+		realDevice.uninstallPackage(applicationId)
 		logger.info("Installing package: ${oldApk}")
 		realDevice.installPackage(oldApk.absolutePath, false)
 	}
 
 	private fun pushData(realDevice: IDevice, debugVariant: ApplicationVariant, version: String) {
+		val applicationId = debugVariant.applicationId.get()
 		val localData =
-			File("${System.getenv("RELEASE_HOME")}/android/${debugVariant.applicationId}@${version}d+debug-data.zip")
+			File("${System.getenv("RELEASE_HOME")}/android/${applicationId}@${version}d+debug-data.zip")
 		logger.info("Pushing ${localData}")
 		@Suppress("SdCardPath") // False positive, this is Gradle code not Android.
 		realDevice.pushFile(localData.absolutePath, "/sdcard/Download/data.zip")
@@ -150,8 +153,11 @@ abstract class UpgradeTestTask : DefaultTask() {
 	}
 }
 
-val BaseVariantData.services: VariantServices
-	get() = BaseVariantData::class.java
-		.getDeclaredField("services")
+val Variant.services: VariantServices
+	get() = ComponentImpl::class.java
+		.getDeclaredField("internalServices")
 		.apply { isAccessible = true }
 		.get(this) as VariantServices
+
+val Artifacts.apk: File
+	get() = this.get(SingleArtifact.APK).get().asFileTree.singleFile
