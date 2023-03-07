8d8b2f162399d113d912e32afd8c280d0465b41d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-07 21:01:54 +0000

    Revert

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.root.gradle.kts
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.root.gradle.kts
@@ -1,10 +1,6 @@
-import net.twisterrob.inventory.build.tests.GenerateUtpDependenciesTask
-
 plugins {
 	id("net.twisterrob.root")
 	id("net.twisterrob.quality")
 	id("org.gradle.idea")
 	id("net.twisterrob.inventory.build.allprojects")
 }
-
-tasks.register<GenerateUtpDependenciesTask>("generateUTPDependencies")
