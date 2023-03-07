51b7d6a5c00732885805425978c7f6c0ed11c8d8
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-07 21:01:02 +0000

    UTP sources

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.root.gradle.kts
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.root.gradle.kts
@@ -1,6 +1,10 @@
+import net.twisterrob.inventory.build.tests.GenerateUtpDependenciesTask
+
 plugins {
 	id("net.twisterrob.root")
 	id("net.twisterrob.quality")
 	id("org.gradle.idea")
 	id("net.twisterrob.inventory.build.allprojects")
 }
+
+tasks.register<GenerateUtpDependenciesTask>("generateUTPDependencies")
