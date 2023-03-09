70755d535eb9183b2cfc8f91221dbd76d18e0dd8
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-09 11:25:55 +0000

    Deduplicate code

--- tools/transform/build.gradle.kts
+++ tools/transform/build.gradle.kts
@@ -18,94 +18,82 @@ tasks.check.configure {
 
 @Suppress("UnstableApiUsage")
 testing {
-	@Suppress("UNUSED_VARIABLE")
 	suites {
 		named<JvmTestSuite>("test").configure {
 			// Keep defaults.
 		}
-		register<JvmTestSuite>("javaIntegrationTest") {
-			testType.set("${TestSuiteType.INTEGRATION_TEST}-java")
+		registerIntegrationTest("java") {
 			dependencies {
 				// None, use default from JRE bootclasspath.
 			}
-			targets {
-				configureEach {
-					testTask.configure {
-						systemProperty("net.twisterrob.inventory.transform.name", "java")
-					}
-				}
-			}
 		}
-		register<JvmTestSuite>("xalanIntegrationTest") {
-			testType.set("${TestSuiteType.INTEGRATION_TEST}-xalan")
+		registerIntegrationTest("xalan") {
 			dependencies {
 				implementation(libs.xml.xalan)
 			}
-			targets {
-				configureEach {
-					testTask.configure {
-						systemProperty("net.twisterrob.inventory.transform.name", "xalan")
-					}
-				}
-			}
 		}
-		register<JvmTestSuite>("saxonIntegrationTest") {
-			testType.set("${TestSuiteType.INTEGRATION_TEST}-saxon")
+		registerIntegrationTest("saxon") {
 			dependencies {
 				implementation(libs.xml.saxon)
 			}
-			targets {
-				configureEach {
-					testTask.configure {
-						systemProperty("net.twisterrob.inventory.transform.name", "saxon")
+		}
+	}
+}
+
+@Suppress("UnstableApiUsage")
+fun NamedDomainObjectContainerScope<TestSuite>.registerIntegrationTest(
+	name: String,
+	configure: JvmTestSuite.() -> Unit
+) {
+	register<JvmTestSuite>("${name}IntegrationTest") {
+		testType.set("${TestSuiteType.INTEGRATION_TEST}-${name}")
+		useJUnit(libs.versions.test.junit4)
+		dependencies {
+			implementation(project())
+		}
+		sources {
+			java {
+				srcDir("src/integrationTest/java")
+			}
+		}
+		targets {
+			configureEach {
+				integrationTests.configure { dependsOn(testTask) }
+				testTask.configure {
+					enableAssertions = true
+					//val dataXml = file("${rootDir}/../temp/test/data.xml")
+					systemProperty("net.twisterrob.inventory.transform.name", name)
+					fun Test.inputFile(prop: String, file: File) {
+						inputs.file(file)
+							.withPropertyName(prop)
+							.withPathSensitivity(PathSensitivity.RELATIVE)
+						systemProperty(prop, file)
 					}
+					inputFile(
+						"net.twisterrob.inventory.transform.xml",
+						project(":android:backup").file("src/main/assets/demo.xml")
+					)
+					inputFile(
+						"net.twisterrob.inventory.transform.xsd",
+						project(":android:backup").file("src/main/assets/data.xml.xsd")
+					)
+					inputFile(
+						"net.twisterrob.inventory.transform.xslt.html",
+						project(":android:backup").file("src/main/assets/data.html.xslt")
+					)
+					inputFile(
+						"net.twisterrob.inventory.transform.xslt.csv",
+						project(":android:backup").file("src/main/assets/data.csv.xslt")
+					)
+					val transformDir = layout.buildDirectory.file("transformed")
+					outputs.dir(transformDir).withPropertyName("transformDir")
+					systemProperty(
+						"net.twisterrob.inventory.transform.output",
+						transformDir.get().asFile
+					)
 				}
 			}
 		}
-		withType(JvmTestSuite::class)
-			.matching { it.testType.get().startsWith("${TestSuiteType.INTEGRATION_TEST}-") }
-			.configureEach {
-				useJUnit(libs.versions.test.junit4)
-				dependencies {
-					implementation(project())
-				}
-				sources {
-					java {
-						srcDir("src/integrationTest/java")
-					}
-				}
-				targets {
-					configureEach {
-						integrationTests.configure { dependsOn(testTask) }
-						testTask.configure {
-							enableAssertions = true
-							//val dataXml = file("${rootDir}/../temp/test/data.xml")
-							fun Test.inputFile(prop: String, file: File) {
-								this.inputs.file(file).withPropertyName(prop).withPathSensitivity(PathSensitivity.RELATIVE)
-								this.systemProperty(prop, file)
-							}
-							inputFile(
-								"net.twisterrob.inventory.transform.xml",
-								project(":android:backup").file("src/main/assets/demo.xml")
-							)
-							inputFile(
-								"net.twisterrob.inventory.transform.xsd",
-								project(":android:backup").file("src/main/assets/data.xml.xsd")
-							)
-							inputFile(
-								"net.twisterrob.inventory.transform.xslt.html",
-								project(":android:backup").file("src/main/assets/data.html.xslt")
-							)
-							inputFile(
-								"net.twisterrob.inventory.transform.xslt.csv",
-								project(":android:backup").file("src/main/assets/data.csv.xslt")
-							)
-							val transformDir = file("build/transformed")
-							outputs.dir(transformDir).withPropertyName("transformDir")
-							systemProperty("net.twisterrob.inventory.transform.output", transformDir)
-						}
-					}
-				}
-			}
+		configure()
 	}
 }
