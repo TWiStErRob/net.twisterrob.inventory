c226fa462a41dd47c893a7e5f060766d324791f4
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-11 00:50:33 +0000

    Proof of concept, doesn't work because the original task still overlaps with default sourceset

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/database/InventoryDatabasePlugin.kt
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/database/InventoryDatabasePlugin.kt
@@ -1,8 +1,10 @@
 package net.twisterrob.inventory.build.database
 
+import net.twisterrob.gradle.android.androidComponents
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.BasePlugin
+import org.gradle.api.tasks.Copy
 import org.gradle.configurationcache.extensions.capitalized
 import org.gradle.kotlin.dsl.container
 import org.gradle.kotlin.dsl.register
@@ -31,20 +33,33 @@ class InventoryDatabasePlugin : Plugin<Project> {
 			group = BasePlugin.BUILD_GROUP
 		}
 		val allTasksClean = project.tasks.register("cleanGenerateDataBase")
-		project.afterEvaluate {
-			entities.all {
-				val entity: InventoryDatabaseEntity = this
-				//println "Creating task for ${entity.name} (${entity.input} --${entity.conversion}--> ${entity.output})"
-				val genDBTaskName = "generateDataBase${entity.name.capitalized()}"
-				val task = project.tasks.register<InventoryDatabaseTask>(genDBTaskName) {
-					this.input.convention(entity.input)
-					this.output.convention(entity.output)
-					this.conversion.convention(entity.conversion)
-					this.iconFolder.convention(entity.iconFolder)
+		entities.configureEach {
+			val entity: InventoryDatabaseEntity = this
+			//println "Creating task for ${entity.name} (${entity.input} --${entity.conversion}--> ${entity.output})"
+			val genDBTaskName = "generateDataBase${entity.name.capitalized()}"
+			val task = project.tasks.register<InventoryDatabaseTask>(genDBTaskName) {
+				this.input.convention(entity.input)
+				this.output.convention(entity.output)
+				this.conversion.convention(entity.conversion)
+				this.iconFolder.convention(entity.iconFolder)
+			}
+			allTasks.configure { dependsOn(task) }
+			// clean task is automagically generated for every task that has output
+			allTasksClean.configure { dependsOn("clean${task.name.capitalized()}") }
+			val copyTask = project.tasks.register<Copy>("copy") {
+				from(task)
+				into(project.layout.buildDirectory.dir("generated/assets_database_entities"))
+			}
+			project.androidComponents.apply {
+				onVariants { variant ->
+					variant.sources.assets?.addGeneratedSourceDirectory(copyTask) { task ->
+						project
+							.objects
+							.directoryProperty()
+							//.fileValue(task.destinationDir)
+							.fileProvider(copyTask.map { task.destinationDir })
+					}
 				}
-				allTasks.configure { dependsOn(task) }
-				// clean task is automagically generated for every task that has output
-				allTasksClean.configure { dependsOn("clean${task.name.capitalized()}") }
 			}
 		}
 	}
