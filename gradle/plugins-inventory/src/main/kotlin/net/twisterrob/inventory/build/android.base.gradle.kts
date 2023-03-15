df6c5b8f78f1c2baea9d11d2a88be3bfa504e9aa
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-15 19:43:59 +0000

    Blind fix

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/android.base.gradle.kts
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/android.base.gradle.kts
@@ -18,5 +18,16 @@ android {
 		checkReleaseBuilds = false
 		baseline = rootDir.resolve("config/lint/lint-baseline-${project.name}.xml")
 		lintConfig = rootDir.resolve("config/lint/lint.xml")
+
+		/*
+		 * Work around
+		 * > CannotEnableHidden: Issue Already Disabled
+		 * > ../../../android: Issue StopShip was configured with severity fatal in android,
+		 * > but was not enabled (or was disabled) in library annotations
+		 * > Any issues that are specifically disabled in a library cannot be re-enabled in a dependent project.
+		 * > To fix this you need to also enable the issue in the library project.
+		 * Strangely even though both projects have my plugin applied which adds the fatal.
+		 */
+		fatal.remove("StopShip")
 	}
 }
