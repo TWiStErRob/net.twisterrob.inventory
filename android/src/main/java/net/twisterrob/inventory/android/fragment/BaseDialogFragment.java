792bf91b8756fbbbd95699f9894413faa2ce3caa
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-16 09:39:40 +0000

    Suppress retainInstance deprecations

--- android/src/main/java/net/twisterrob/inventory/android/fragment/BaseDialogFragment.java
+++ android/src/main/java/net/twisterrob/inventory/android/fragment/BaseDialogFragment.java
@@ -3,6 +3,7 @@ package net.twisterrob.inventory.android.fragment;
 import androidx.fragment.app.DialogFragment;
 
 public class BaseDialogFragment extends DialogFragment {
+	@SuppressWarnings("deprecation") // https://github.com/TWiStErRob/net.twisterrob.inventory/issues/258
 	@Override public void onDestroyView() {
 		// http://stackoverflow.com/questions/8235080/fragments-dialogfragment-and-screen-rotation
 		// https://issuetracker.google.com/issues/36929400
