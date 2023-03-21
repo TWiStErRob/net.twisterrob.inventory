d54f1b177bd6c5ba6df4ff883e8620da6e4939a0
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-21 23:39:29 +0000

    Use dynamic version catalog resolution

--- gradle/platform/build.gradle.kts
+++ gradle/platform/build.gradle.kts
@@ -15,54 +15,15 @@ dependencies {
 	api(platform(libs.kotlin.bom))
 
 	constraints {
-		apiWithKtx(libs.androidx.activity)
-		apiWithKtx(libs.androidx.annotation)
-		apiWithKtx(libs.androidx.annotationExperimental)
-		apiWithKtx(libs.androidx.appcompat)
-		apiWithKtx(libs.androidx.appcompatResources)
-		apiWithKtx(libs.androidx.archCoreCommon)
-		apiWithKtx(libs.androidx.archCoreRuntime)
-		apiWithKtx(libs.androidx.cardview)
-		apiWithKtx(libs.androidx.collection)
-		apiWithKtx(libs.androidx.concurrentFutures)
-		apiWithKtx(libs.androidx.constraintlayout)
-		apiWithKtx(libs.androidx.coordinatorlayout)
-		apiWithKtx(libs.androidx.core)
-		apiWithKtx(libs.androidx.cursoradapter)
-		apiWithKtx(libs.androidx.customview)
-		apiWithKtx(libs.androidx.documentfile)
-		apiWithKtx(libs.androidx.drawerlayout)
-		apiWithKtx(libs.androidx.dynamicanimation)
-		apiWithKtx(libs.androidx.exifinterface)
-		apiWithKtx(libs.androidx.fragment)
-		apiWithKtx(libs.androidx.interpolator)
-		apiWithKtx(libs.androidx.lifecycleCommon)
-		apiWithKtx(libs.androidx.lifecycleRuntime)
-		apiWithKtx(libs.androidx.lifecycleViewModel)
-		apiWithKtx(libs.androidx.loader)
-		apiWithKtx(libs.androidx.localbroadcastmanager)
-		apiWithKtx(libs.androidx.material)
-		apiWithKtx(libs.androidx.multidex)
-		apiWithKtx(libs.androidx.preference)
-		apiWithKtx(libs.androidx.print)
-		apiWithKtx(libs.androidx.recyclerview)
-		apiWithKtx(libs.androidx.savedstate)
-		apiWithKtx(libs.androidx.slidingpanelayout)
-		apiWithKtx(libs.androidx.swiperefreshlayout)
-		apiWithKtx(libs.androidx.transition)
-		apiWithKtx(libs.androidx.vectordrawable)
-		apiWithKtx(libs.androidx.viewpager)
-		apiWithKtx(libs.androidx.viewpager2)
+		val libsVC = versionCatalogs.named("libs")
+		val (androidxTest, androidxProd) = libsVC
+			.libraryAliases
+			.filter { it.startsWith("androidx.") }
+			.partition { it.startsWith("androidx.test.") }
 
-		api(libs.androidx.test.junit)
-		api(libs.androidx.test.core)
-		api(libs.androidx.test.runner)
-		api(libs.androidx.test.rules)
-		api(libs.androidx.test.monitor)
-		api(libs.androidx.test.espressoCore)
-		api(libs.androidx.test.espressoIntents)
-		api(libs.androidx.test.espressoContrib)
-		api(libs.androidx.test.espressoIdlingResource)
+		androidxProd.forEach { apiWithKtx(libsVC.findLibrary(it).get()) }
+		androidxTest.forEach { api(libsVC.findLibrary(it).get()) }
+
 		api(libs.test.robolectric)
 		api(libs.test.robolectricMultidexShadows)
 	}
