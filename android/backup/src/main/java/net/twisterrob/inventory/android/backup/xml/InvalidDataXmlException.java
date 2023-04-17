78e7e9c53a3726961c87e2904af3ca466ddd9896
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-17 16:09:45 +0100

    Reasonable error message when importing broken XML.

---
+++ android/backup/src/main/java/net/twisterrob/inventory/android/backup/xml/InvalidDataXmlException.java
@@ -0,0 +1,9 @@
+package net.twisterrob.inventory.android.backup.xml;
+
+public class InvalidDataXmlException extends Exception {
+	private static final long serialVersionUID = 8273997882913644027L;
+
+	public InvalidDataXmlException(Throwable cause) {
+		super(cause);
+	}
+}
