8b5a8b8cb2f90f2fab0962928806f1741358b08f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-20 13:58:12 +0100

    Merge files

--- android/space/src/main/kotlin/net/twisterrob/inventory/android/activity/space/manageSpace.kt
+++ android/space/src/main/kotlin/net/twisterrob/inventory/android/activity/space/manageSpace.kt
@@ -1,9 +1,14 @@
 package net.twisterrob.inventory.android.activity.space
 
 import android.content.Context
+import android.text.format.Formatter
 import android.widget.Toast
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.reduce
 import javax.inject.Inject
@@ -60,3 +65,49 @@ internal class ManageSpaceEffectHandler @Inject constructor(
 		}
 	}
 }
+
+class GetSizesUseCase @Inject constructor(
+) : UseCase<Unit, SizesDomain> {
+	override suspend fun execute(input: Unit): SizesDomain = withContext(Dispatchers.IO) {
+		delay(1000)
+		SizesDomain(
+			imageCache = (Math.random() * 1000).toLong(),
+			database = 2,
+			freelist = 3,
+			searchIndex = 4,
+			allData = 5,
+		)
+	}
+}
+
+data class SizesDomain(
+	val imageCache: Long,
+	val database: Long,
+	val freelist: Long,
+	val searchIndex: Long,
+	val allData: Long,
+)
+
+internal class SizesDomainToStateMapper @Inject constructor(
+	@ApplicationContext private val context: Context,
+) : Mapper<SizesDomain, SizesState> {
+	override fun map(input: SizesDomain): SizesState =
+		SizesState(
+			imageCache = input.imageCache.formatFileSize(),
+			database = input.database.formatFileSize(),
+			freelist = input.freelist.formatFileSize(),
+			searchIndex = input.searchIndex.formatFileSize(),
+			allData = input.allData.formatFileSize(),
+		)
+
+	private fun Long.formatFileSize(): String =
+		Formatter.formatFileSize(context, this)
+}
+
+internal data class SizesState(
+	val imageCache: String,
+	val database: String,
+	val freelist: String,
+	val searchIndex: String,
+	val allData: String,
+)
