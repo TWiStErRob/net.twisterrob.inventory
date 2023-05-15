257761552265c1f3361e908aeaa6ec7ef4b2cbbf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-15 11:25:23 +0100

    Big buttons, KISS

--- android/backup/src/main/java/net/twisterrob/inventory/android/fragment/BackupControllerFragment.java
+++ android/backup/src/main/java/net/twisterrob/inventory/android/fragment/BackupControllerFragment.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
@@ -21,7 +22,6 @@ import net.twisterrob.android.utils.tools.DialogTools;
 import net.twisterrob.inventory.android.Constants;
 import net.twisterrob.inventory.android.backup.R;
 import net.twisterrob.inventory.android.backup.concurrent.BackupService;
-import net.twisterrob.inventory.android.backup.ui.BackupActionView;
 import net.twisterrob.inventory.android.content.InventoryContract;
 
 public class BackupControllerFragment extends BaseFragment<BackupControllerFragment.BackupEvents> {
@@ -74,9 +74,9 @@ public class BackupControllerFragment extends BaseFragment<BackupControllerFragm
 			}
 	);
 
-	private BackupActionView importButton;
-	private BackupActionView exportButton;
-	private BackupActionView sendButton;
+	private Button importButton;
+	private Button exportButton;
+	private Button sendButton;
 
 	@Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
