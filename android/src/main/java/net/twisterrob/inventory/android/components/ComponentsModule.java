e3044c8a387a8198be1d4faa075441b066e642ad
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-23 15:04:08 +0100

    Add BaseComponent to Dagger

--- android/src/main/java/net/twisterrob/inventory/android/components/ComponentsModule.java
+++ android/src/main/java/net/twisterrob/inventory/android/components/ComponentsModule.java
@@ -1,12 +1,24 @@
 package net.twisterrob.inventory.android.components;
 
+import android.content.Context;
+
 import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
+
+import net.twisterrob.inventory.android.BaseComponent;
 
 @Module
 @InstallIn(SingletonComponent.class)
-interface ComponentsModule {
-	@Binds ErrorMapper bindErrorMapper(ErrorMapperImpl impl);
+abstract class ComponentsModule {
+	@Binds
+	abstract ErrorMapper bindErrorMapper(ErrorMapperImpl impl);
+
+	@Provides
+	static BaseComponent provideBaseComponent(@ApplicationContext Context context) {
+		return BaseComponent.get(context);
+	}
 }
