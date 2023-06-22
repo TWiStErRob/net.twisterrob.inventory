3af0652014e7dca7bcd32d9d2d771dc89a627721
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-06-22 13:32:44 +0100

    Stricter execution

--- android/data/svg/src/androidTest/java/net/twisterrob/inventory/android/data/svg/DumpImages.java
+++ android/data/svg/src/androidTest/java/net/twisterrob/inventory/android/data/svg/DumpImages.java
@@ -2,15 +2,20 @@ package net.twisterrob.inventory.android.data.svg;
 
 import java.io.*;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.*;
 
 import org.junit.*;
 import org.junit.runner.RunWith;
+import org.junit.runners.model.MultipleFailureException;
 import org.slf4j.*;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeFalse;
 
 import android.content.Context;
@@ -29,6 +34,7 @@ import com.caverock.androidsvg.SVG;
 import androidx.annotation.*;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.io.FileTestStorage;
 import androidx.test.platform.io.OutputDirCalculator;
 import androidx.test.platform.io.PlatformTestStorage;
 import androidx.test.platform.io.PlatformTestStorageRegistry;
@@ -48,7 +54,7 @@ public class DumpImages {
 	private static final Logger LOG = LoggerFactory.getLogger(DumpImages.class);
 
 	@Test
-	public void test() throws IOException, IllegalAccessException, InterruptedException {
+	public void test() throws Throwable {
 		PlatformTestStorage storage = PlatformTestStorageRegistry.getInstance();
 		assumeFalse(
 				"PlatformTestStorage goes nowhere!",
@@ -69,41 +75,66 @@ public class DumpImages {
 				"icon_preview",
 				"icon_helpers"
 		));
-		for (Field field : fields) {
-			final String name = field.getName();
-			if (exclusions.contains(name)) continue;
-			final File target = new File(dir, name + ".png");
+		List<Future<?>> jobs = new ArrayList<>();
+		for (final Field field : fields) {
+			if (exclusions.contains(field.getName())) {
+				continue;
+			}
+			final File target = new File(dir, field.getName() + ".png");
 			@RawRes final int rawId = (Integer)field.get(null);
-			service.submit(new Runnable() {
+			jobs.add(service.submit(new Runnable() {
 				@Override public void run() {
 					try {
 						saveSVG(context, rawId, target);
 						LOG.info("Generated {}", target.getAbsolutePath());
 					} catch (Exception ex) {
+						String fullName = field.getDeclaringClass().getName() + "." + field.getName();
+						RuntimeException wrapped =
+								new IllegalStateException("Couldn't generate " + fullName, ex);
 						try {
 							String stackTrace = ObjectTools.getFullStackTrace(ex);
 							IOTools.writeAll(new FileOutputStream(target), stackTrace);
-							LOG.warn("{} failed: {}", name, ex, ex);
+							LOG.warn(wrapped.getMessage(), ex);
 						} catch (IOException ex2) {
 							// Shouldn't happen normally.
-							throw new IllegalStateException(ex2);
+							throw new IllegalStateException("Failed to write exception", ex2);
 						}
+						throw wrapped;
 					}
 				}
-			});
+			}));
 		}
 		service.shutdown();
-		service.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
-
+		assertTrue(service.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS));
 		File zip = new File(dir.getParentFile(), dir.getName() + ".zip");
 		IOTools.zip(zip, false, dir);
 		LOG.info("ZIP: {}", zip.getAbsolutePath());
 		String fileName = "svg_" + VERSION.SDK_INT + ".zip";
 		OutputStream saved = storage.openOutputFile(fileName);
 		IOTools.copyStream(new FileInputStream(zip), saved);
-		// Guess where storage.openOutputFile() puts the file.
-		File target = new File(new OutputDirCalculator().getOutputDir(), fileName);
-		LOG.error("adb pull {}", target.getAbsolutePath());
+		if (storage instanceof FileTestStorage) {
+			// Guess where storage.openOutputFile() puts the file.
+			File target = new File(new OutputDirCalculator().getOutputDir(), fileName);
+			LOG.error("adb pull {}", target.getAbsolutePath());
+		} else {
+			LOG.error(
+					"{} should be available in build/outputs/(connected|managed_device)_android_test_additional_output/...",
+					fileName
+			);
+		}
+		failTestIfAnyJobFailed(jobs);
+	}
+
+	private static void failTestIfAnyJobFailed(List<Future<?>> jobs) throws Exception {
+		List<Throwable> errors = new LinkedList<>();
+		for (Future<?> job : jobs) {
+			try {
+				job.get();
+			} catch (ExecutionException ex) {
+				errors.add(ex.getCause());
+			}
+		}
+		MultipleFailureException.assertEmpty(errors);
 	}
 
 	private static void save(@NonNull Bitmap bitmap, @NonNull File file) throws IOException {
