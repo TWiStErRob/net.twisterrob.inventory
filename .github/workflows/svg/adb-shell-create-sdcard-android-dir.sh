64f62d06dd3dd41053cd79bb7a56ce93b487ca06
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-06-28 12:50:33 +0100

    Keep double quotes, because otherwise it triggers https://github.com/koalaman/shellcheck/wiki/SC2016

--- .github/workflows/svg/adb-shell-create-sdcard-android-dir.sh
+++ .github/workflows/svg/adb-shell-create-sdcard-android-dir.sh
@@ -11,12 +11,12 @@ API_LEVEL=$1
 if [ "${API_LEVEL}" -lt 29 ]; then
     # Satisfy AGP's findAdditionalTestOutputDirectoryOnDevice -> queryAdditionalTestOutputLocation
     # so API 21-28 can generate additional output.
-    adb shell 'mkdir /sdcard/Android'
+    adb shell "mkdir /sdcard/Android"
     # Tell the Android system's MediaProvider that we added a directory.
     # This will ensure that `content query --uri content://media/external/file` finds the new folder.
     # Note: API 21-22 does not support readlink -e.
     # Note: API 27 does not support readlink without root (21-26,28-34 is ok).
-    adb shell 'content insert --uri content://media/external/file --bind _data:s:$(su root readlink -f /sdcard)/Android'
+    adb shell "content insert --uri content://media/external/file --bind _data:s:\$(su root readlink -f /sdcard)/Android"
 else
     echo "API 29+ (${API_LEVEL}) does not need to create /sdcard/Android"
 fi
