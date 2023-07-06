7e62bbb69f40fc4af5f536741b742a195d6bb7b7
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-06 19:39:02 +0100

    Remove unused code from CategoryDTO (paired recursion)

--- android/src/main/java/net/twisterrob/inventory/android/content/model/CategoryDTO.java
+++ android/src/main/java/net/twisterrob/inventory/android/content/model/CategoryDTO.java
@@ -4,10 +4,8 @@ import java.util.*;
 
 import android.annotation.SuppressLint;
 import android.content.*;
-import android.content.res.Resources.NotFoundException;
 import android.database.Cursor;
 import android.net.Uri;
-import android.text.SpannableStringBuilder;
 
 import androidx.annotation.*;
 
@@ -16,7 +14,6 @@ import net.twisterrob.inventory.android.BuildConfig;
 import net.twisterrob.inventory.android.categories.cache.CategoryCache;
 import net.twisterrob.inventory.android.categories.cache.CategoryCacheProvider;
 import net.twisterrob.inventory.android.content.contract.*;
-import net.twisterrob.inventory.android.view.ChangeTypeDialog;
 
 public class CategoryDTO extends ImagedDTO {
 	private static final Uri APP_RESOURCE_RAW = Uri.parse("android.resource://" + BuildConfig.APPLICATION_ID + "/raw/");
@@ -49,57 +46,6 @@ public class CategoryDTO extends ImagedDTO {
 	@Override
 	public @NonNull String toString() {
 		return String.format(Locale.ROOT, "Category #%1$d: '%2$s' in %3$s", id, name, parentID);
-	}
-
-	public static @Nullable CharSequence getKeywords(@NonNull Context context, @NonNull String categoryName,
-			boolean deep) { // STOPSHIP remove
-		try {
-			CharSequence keywords = ResourceTools.getText(context, ResourceNames.getKeywordsName(categoryName));
-			if (deep) {
-				SpannableStringBuilder more = new SpannableStringBuilder(keywords);
-				for (String sub : getCache(context).getChildren(categoryName)) {
-					if (more.length() > 0) {
-						more.append(",\n");
-					}
-					TextTools.appendBold(more, ResourceTools.getText(context, sub));
-
-					CharSequence extended = getKeywordsExtended(context, sub);
-					if (extended != null) {
-						more.append(" (").append(extended).append(")");
-					}
-				}
-				keywords = more;
-			}
-			return keywords.length() != 0? keywords : null;
-		} catch (NotFoundException ex) {
-			return null;
-		}
-	}
-
-	public static @Nullable CharSequence getKeywordsExtended(@NonNull Context context, @NonNull String categoryName) { // STOPSHIP remove
-		SpannableStringBuilder keywords = new SpannableStringBuilder();
-
-		CharSequence myKeywords = getKeywords(context, categoryName, false);
-		if (myKeywords != null) {
-			keywords.append(myKeywords);
-		}
-
-		Collection<String> children = getCache(context).getChildren(categoryName);
-		if (!children.isEmpty()) {
-			if (0 < keywords.length()) {
-				keywords.append("; ");
-			}
-			TextTools.appendBold(keywords, "more");
-			keywords.append(": ");
-			for (Iterator<String> it = children.iterator(); it.hasNext(); ) {
-				TextTools.appendItalic(keywords, ResourceTools.getText(context, it.next()));
-				if (it.hasNext()) {
-					keywords.append(", ");
-				}
-			}
-		}
-
-		return keywords.length() != 0? keywords : null;
 	}
 
 	@SuppressLint("StaticFieldLeak")
