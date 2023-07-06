92fba615783ce562b7aa4a9637cedf7a29e5bbaf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-06 18:33:19 +0100

    Add some missing Hilt folders to the IDE sources

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/hilt.gradle.kts
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/build/hilt.gradle.kts
@@ -1,16 +1,18 @@
 package net.twisterrob.inventory.build
 
 //import net.twisterrob.inventory.build.dsl.android
+import com.android.build.api.variant.ComponentIdentity
+import com.android.build.api.variant.HasAndroidTest
+import net.twisterrob.gradle.android.androidComponents
 import net.twisterrob.inventory.build.dsl.hilt
 import net.twisterrob.inventory.build.dsl.libs
-import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.kotlin.dsl.withType
 
 plugins {
 	id("org.gradle.java-base")
 	id("com.google.dagger.hilt.android")
 	id("org.jetbrains.kotlin.android")
 	id("org.jetbrains.kotlin.kapt")
+	id("org.gradle.idea")
 }
 
 @Suppress("UnstableApiUsage")
@@ -61,3 +63,36 @@ hilt {
 //		}
 //	}
 //}
+
+androidComponents.onVariants { variant ->
+	// FIXME Doesn't tie the generated sources to the variant ([main] generated sources root)
+	// So the files are full of errors and indexing doesn't link them up either.
+	// Anyway, at least they're navigable by hand and full text search.
+	val addGeneratedSources: (File) -> Unit = { dir: File ->
+		// Have to add it to both so that it's marked correctly at
+		// org.gradle.plugins.ide.internal.tooling.IdeaModelBuilder.srcDirs.
+		idea.module.sourceDirs.add(dir)
+		idea.module.generatedSourceDirs.add(dir)
+	}
+	variant.componentTreesDir.let(addGeneratedSources)
+	variant.componentSourcesDir.let(addGeneratedSources)
+	variant.unitTest?.componentTreesDir?.let(addGeneratedSources)
+	variant.unitTest?.componentSourcesDir?.let(addGeneratedSources)
+	(variant as? HasAndroidTest)?.androidTest?.componentTreesDir?.let(addGeneratedSources)
+	(variant as? HasAndroidTest)?.androidTest?.componentSourcesDir?.let(addGeneratedSources)
+}
+
+val ComponentIdentity.componentTreesDir: File
+	get() = hiltGenerated("component_tree").dir(name).get().asFile
+
+val ComponentIdentity.componentSourcesDir: File
+	get() = hiltGenerated("component_sources").dir(name).get().asFile
+
+/**
+ * @see dagger.hilt.android.plugin.HiltGradlePlugin
+ */
+fun Project.hiltGenerated(path: String): Provider<Directory> =
+	layout.buildDirectory.dir("generated/hilt").dir(path)
+
+fun Provider<Directory>.dir(name: String): Provider<Directory> =
+	map { it.dir(name) }
