TWiStErRob/net.twisterrob.inventory

commit 9742849dd0c548fb357fe566bc9896aec17bf3c8

Make all Activities/Fragments @AndroidEntryPoint and inject CategoryCache (#290)

android/src/main/java/net/twisterrob/inventory/android/content/model/CategoryVisuals.java
@@ -1,0 +1,102 @@
+package net.twisterrob.inventory.android.content.model;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.inject.Inject;
+
+import android.content.Context;
+import android.content.res.Resources.NotFoundException;
+import android.text.SpannableStringBuilder;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+import net.twisterrob.android.utils.tools.ResourceTools;
+import net.twisterrob.android.utils.tools.TextTools;
+import net.twisterrob.inventory.android.categories.cache.CategoryCache;
+import net.twisterrob.inventory.android.content.contract.ResourceNames;
+import net.twisterrob.inventory.android.view.ChangeTypeDialog;
+
+public class CategoryVisuals {
+
+	private final @NonNull Context context;
+	private final @NonNull CategoryCache cache;
+
+	@Inject
+	public CategoryVisuals(
+			@ApplicationContext @NonNull Context context,
+			@NonNull CategoryCache cache
+	) {
+		this.context = context;
+		this.cache = cache;
+	}
+
+	public @Nullable CharSequence getKeywords(@NonNull String categoryName) {
+		return getKeywords(categoryName, false);
+	}
+
+	public @Nullable CharSequence getKeywords(@NonNull String categoryName, boolean deep) {
+		try {
+			CharSequence keywords = ResourceTools.getText(context, ResourceNames.getKeywordsName(categoryName));
+			if (deep) {
+				SpannableStringBuilder more = new SpannableStringBuilder(keywords);
+				for (String sub : cache.getChildren(categoryName)) {
+					if (more.length() > 0) {
+						more.append(",\n");
+					}
+					TextTools.appendBold(more, ResourceTools.getText(context, sub));
+
+					CharSequence extended = getKeywordsExtended(sub);
+					if (extended != null) {
+						more.append(" (").append(extended).append(")");
+					}
+				}
+				keywords = more;
+			}
+			return keywords.length() != 0? keywords : null;
+		} catch (NotFoundException ex) {
+			return null;
+		}
+	}
+
+	public @Nullable CharSequence getKeywordsExtended(@NonNull String categoryName) {
+		SpannableStringBuilder keywords = new SpannableStringBuilder();
+
+		CharSequence myKeywords = getKeywords(categoryName, false);
+		if (myKeywords != null) {
+			keywords.append(myKeywords);
+		}
+
+		Collection<String> children = cache.getChildren(categoryName);
+		if (!children.isEmpty()) {
+			if (0 < keywords.length()) {
+				keywords.append("; ");
+			}
+			TextTools.appendBold(keywords, "more");
+			keywords.append(": ");
+			for (Iterator<String> it = children.iterator(); it.hasNext(); ) {
+				TextTools.appendItalic(keywords, ResourceTools.getText(context, it.next()));
+				if (it.hasNext()) {
+					keywords.append(", ");
+				}
+			}
+		}
+
+		return keywords.length() != 0? keywords : null;
+	}
+
+	public @Nullable CharSequence getDescription(@NonNull String categoryName) {
+		try {
+			return ResourceTools.getText(context, ResourceNames.getDescriptionName(categoryName));
+		} catch (NotFoundException ex) {
+			return null;
+		}
+	}
+
+	public void showKeywords(long categoryID) {
+		CharSequence keywords = getKeywords(cache.getCategoryKey(categoryID), true);
+		ChangeTypeDialog.showKeywords(context, cache.getCategoryPath(categoryID), keywords);
+	}
+}

android/src/main/java/net/twisterrob/inventory/android/fragment/data/BaseEditFragment.java
@@ -2,6 +2,8 @@ package net.twisterrob.inventory.android.fragment.data;
 
 import java.io.File;
 
+import javax.inject.Inject;
+
 import org.slf4j.*;
 
 import android.annotation.*;
@@ -51,6 +53,7 @@ import net.twisterrob.inventory.android.view.*;
 import net.twisterrob.inventory.android.view.ChangeTypeDialog.Variants;
 import net.twisterrob.inventory.android.view.adapters.TypeAdapter;
 
+// Every subclass must have @AndroidEntryPoint or otherwise initialize @Inject fields.
 public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSingleLoaderFragment<T> {
 	private static final Logger LOG = LoggerFactory.getLogger(BaseEditFragment.class);
 	public static final String EDIT_IMAGE = "editImageOnStartup";
@@ -59,6 +62,9 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 	protected static final String DYN_NameHintResource = "nameHint";
 	protected static final String DYN_DescriptionHintResource = "descriptionHint";
 	private static final int REQUEST_CODE_GET_PICTURE = 0x3245;
+
+	@Inject protected CategoryVisuals visuals;
+	@Inject protected CategoryCache cache;
 
 	private boolean isRestored;
 	private @Nullable Uri restoredImage;
@@ -149,13 +155,12 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 						}
 						@Override public CharSequence getTypeName(Cursor cursor) {
 							long categoryID = DatabaseTools.getLong(cursor, Category.ID);
-							CategoryCache cache = CategoryDTO.getCache(requireContext());
 							return cache.getCategoryPath(categoryID);
 						}
 						@Override public CharSequence getKeywords(Cursor cursor) {
 							long categoryID = DatabaseTools.getLong(cursor, Category.ID);
-							CategoryCache cache = CategoryDTO.getCache(requireContext());
-							return CategoryDTO.getKeywords(requireContext(), cache.getCategoryKey(categoryID), true);
+							String categoryKey = cache.getCategoryKey(categoryID);
+							return visuals.getKeywords(categoryKey, true);
 						}
 					}, type.getSelectedItemId());
 				}
@@ -251,13 +256,13 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 		hint.setLayoutManager(new LinearLayoutManager(requireContext()));
 		hint.addOnItemTouchListener(new NestedScrollableRecyclerViewListener(hint));
 		if (this instanceof ItemEditFragment) {
-			hinter = new Hinter(requireContext(), new CategorySelectedEvent() {
+			hinter = new Hinter(requireContext(), cache, new CategorySelectedEvent() {
 				@Override public void categorySelected(long categoryID) {
 					AndroidTools.selectByID(type, categoryID);
 					Hinter.unhighlight(name.getText());
 				}
 				@Override public void categoryQueried(long categoryID) {
-					CategoryDTO.showKeywords(requireContext(), categoryID);
+					visuals.showKeywords(categoryID);
 				}
 			});
 			hint.setAdapter(hinter.getAdapter());
@@ -302,7 +307,7 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 		// CONSIDER setOnItemLongClickListener is not supported, any way to work around? So user has the same "tooltip" as in ChangeTypeDialog
 		type.setOnLongClickListener(new OnLongClickListener() {
 			@Override public boolean onLongClick(View view) {
-				CategoryDTO.showKeywords(view.getContext(), getTypeId());
+				visuals.showKeywords(getTypeId());
 				return true;
 			}
 		});
@@ -351,7 +356,7 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 				updateHint(name.getText(), true);
 				return true;
 			case R.id.action_category_keywords:
-				CategoryDTO.showKeywords(requireContext(), getTypeId());
+				visuals.showKeywords(getTypeId());
 				return true;
 		}
 		return super.onContextItemSelected(item);

android/backup/src/main/java/net/twisterrob/inventory/android/activity/BackupActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import androidx.annotation.*;
 import androidx.appcompat.app.AlertDialog;
+import dagger.hilt.android.AndroidEntryPoint;
 
 import net.twisterrob.android.utils.tools.*;
 import net.twisterrob.android.utils.tools.DialogTools.PopupCallbacks;
@@ -20,6 +21,7 @@ import net.twisterrob.inventory.android.fragment.BackupControllerFragment;
 
 import static net.twisterrob.inventory.android.content.BroadcastTools.getLocalBroadcastManager;
 
+@AndroidEntryPoint
 public class BackupActivity extends BaseActivity implements BackupControllerFragment.BackupEvents {
 	private static final Logger LOG = LoggerFactory.getLogger(BackupActivity.class);
 	private boolean allowNew;

android/backup/src/main/java/net/twisterrob/inventory/android/fragment/BackupControllerFragment.java
@@ -15,6 +15,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import dagger.hilt.android.AndroidEntryPoint;
 
 import net.twisterrob.android.utils.tools.DialogTools;
 import net.twisterrob.inventory.android.Constants;
@@ -24,6 +25,7 @@ import net.twisterrob.inventory.android.content.CreateOpenableDocument;
 import net.twisterrob.inventory.android.content.InventoryContract;
 import net.twisterrob.inventory.android.content.OpenOpenableDocument;
 
+@AndroidEntryPoint
 public class BackupControllerFragment extends BaseFragment<BackupControllerFragment.BackupEvents> {
 	private static final Logger LOG = LoggerFactory.getLogger(BackupControllerFragment.class);
 

android/backup/src/main/java/net/twisterrob/inventory/android/fragment/BackupProgressFragment.java
@@ -13,6 +13,7 @@ import android.widget.*;
 
 import androidx.annotation.*;
 import androidx.appcompat.app.AlertDialog;
+import dagger.hilt.android.AndroidEntryPoint;
 
 import net.twisterrob.android.utils.tools.*;
 import net.twisterrob.android.utils.tools.DialogTools.PopupCallbacks;
@@ -23,6 +24,7 @@ import net.twisterrob.inventory.android.backup.concurrent.BackupService.LocalBin
 import static net.twisterrob.inventory.android.backup.concurrent.NotificationProgressService.*;
 import static net.twisterrob.inventory.android.content.BroadcastTools.getLocalBroadcastManager;
 
+@AndroidEntryPoint
 public class BackupProgressFragment extends BaseFragment<Void> {
 	private static final Logger LOG = LoggerFactory.getLogger(BackupProgressFragment.class);
 

android/base/src/main/java/net/twisterrob/inventory/android/BaseComponent.java
@@ -1,39 +1,0 @@
-package net.twisterrob.inventory.android;
-
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-
-/**
- * @see net.twisterrob.inventory.android.components.ComponentsModule
- */
-public abstract class BaseComponent {
-
-	/**
-	 * @deprecated inject {@link net.twisterrob.inventory.android.content.Database} directly.
-	 */
-	@SuppressWarnings({"deprecation", "JavadocReference"})
-	@Deprecated
-	public static @NonNull BaseComponent get(@NonNull Context context) {
-		return ((Provider)context.getApplicationContext()).getBaseComponent();
-	}
-
-	/**
-	 * Untyped, so that base doesn't have to depend on database module.
-	 *
-	 * @return net.twisterrob.inventory.android.content.Database
-	 * @deprecated inject {@link net.twisterrob.inventory.android.content.Database} directly.
-	 */
-	@SuppressWarnings("JavadocReference")
-	@Deprecated
-	public abstract @NonNull Object db();
-
-	interface Provider {
-		/**
-		 * @deprecated inject {@link net.twisterrob.inventory.android.content.Database} directly.
-		 */
-		@Deprecated
-		@SuppressWarnings("JavadocReference")
-		BaseComponent getBaseComponent();
-	}
-}

android/base/src/main/kotlin/net/twisterrob/inventory/android/preconditions.kt
@@ -1,0 +1,15 @@
+package net.twisterrob.inventory.android
+
+fun check(value: Boolean) {
+	kotlin.check(value)
+}
+
+fun require(value: Boolean) {
+	kotlin.require(value)
+}
+
+fun <T> checkNotNull(value: T?): T =
+	kotlin.checkNotNull(value)
+
+fun <T> requireNotNull(value: T?): T =
+	kotlin.requireNotNull(value)

android/database/src/main/java/net/twisterrob/inventory/android/categories/cache/CategoryCacheHolder.java
@@ -1,0 +1,118 @@
+package net.twisterrob.inventory.android.categories.cache;
+
+import java.util.Locale;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.StrictMode;
+
+import androidx.annotation.AnyThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityComponent;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+import net.twisterrob.android.utils.tools.AndroidTools;
+import net.twisterrob.android.utils.tools.DatabaseTools;
+import net.twisterrob.inventory.android.PreconditionsKt;
+import net.twisterrob.inventory.android.content.Database;
+import net.twisterrob.inventory.android.content.contract.CommonColumns;
+import net.twisterrob.inventory.android.content.contract.ParentColumns;
+
+@Singleton
+public class CategoryCacheHolder {
+	private static final Logger LOG = LoggerFactory.getLogger(CategoryCacheHolder.class);
+
+	private @Nullable CategoryCache CACHE;
+	private @Nullable Locale lastLocale;
+
+	private final @NonNull Context context;
+	private final @NonNull Database database;
+
+	@Inject
+	public CategoryCacheHolder(
+			@ApplicationContext @NonNull Context context,
+			@NonNull Database database
+	) {
+		this.context = context;
+		this.database = database;
+	}
+
+	@WorkerThread
+	synchronized
+	private @NonNull CategoryCache get(@NonNull Context context) {
+		Locale currentLocale = AndroidTools.getLocale(context.getResources().getConfiguration());
+		if (!currentLocale.equals(lastLocale)) {
+			// TODO externalize this to an explicit call.
+			LOG.info("Locale changed from {} to {}", lastLocale, currentLocale);
+			CategoryCacheImpl cache = new CategoryCacheImpl(context);
+			fillItemCategories(cache);
+			CACHE = cache;
+			lastLocale = currentLocale;
+		}
+		return PreconditionsKt.checkNotNull(CACHE);
+	}
+
+	@SuppressLint("WrongThread")
+	@AnyThread
+	public @NonNull CategoryCache getCacheForCurrentLocale() {
+		StrictMode.ThreadPolicy originalPolicy = StrictMode.allowThreadDiskReads();
+		try {
+			// Initialization will happen only once, after that it's cached.
+			return get(context.getApplicationContext());
+		} finally {
+			StrictMode.setThreadPolicy(originalPolicy);
+		}
+	}
+
+	private void fillPropertyTypes(@NonNull CategoryCacheImpl cache) {
+		fillCategoriesFrom(cache, database.listPropertyTypes());
+	}
+
+	private void fillRoomTypes(@NonNull CategoryCacheImpl cache) {
+		fillCategoriesFrom(cache, database.listRoomTypes());
+	}
+
+	private void fillItemCategories(@NonNull CategoryCacheImpl cache) {
+		fillCategoriesFrom(cache, database.listRelatedCategories(null));
+	}
+
+	private static void fillCategoriesFrom(@NonNull CategoryCacheImpl cache, @NonNull Cursor cursor) {
+		try {
+			while (cursor.moveToNext()) {
+				String categoryName = cursor.getString(cursor.getColumnIndexOrThrow(CommonColumns.NAME));
+				long categoryID = cursor.getLong(cursor.getColumnIndexOrThrow(CommonColumns.ID));
+				Long parentID = DatabaseTools.getOptionalLong(cursor, ParentColumns.PARENT_ID);
+				String categoryIcon = cursor.getString(cursor.getColumnIndexOrThrow(CommonColumns.TYPE_IMAGE));
+				cache.addCategory(categoryName, categoryID, parentID, categoryIcon);
+			}
+		} finally {
+			cursor.close();
+		}
+	}
+
+	@Module
+	// Installing in Activity means it cannot be injected in the Singleton scope,
+	// so whenever a CategoryCache is injected it'll be a short-lived Activity/Fragment or lower.
+	@InstallIn(ActivityComponent.class)
+	public static class CategoryCacheModule {
+
+		// Not scoped, call provider every time to get the latest cache.
+		@Provides @NonNull CategoryCache provideCategoryCache(
+				@NonNull CategoryCacheHolder provider
+		) {
+			return provider.getCacheForCurrentLocale();
+		}
+	}
+}

commit 12124d6c5822f4e28e9203fee2e18fa290392615

Fix crash caused by #290, discovered by pre-launch report

android/src/androidTest/java/net/twisterrob/inventory/android/activity/ItemEditActivityTest_Edit.java
@@ -4,6 +4,9 @@ import org.junit.*;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.not;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import net.twisterrob.inventory.android.activity.data.ItemEditActivity;
@@ -59,5 +62,12 @@ public class ItemEditActivityTest_Edit {
 
 		// changes applied
 		db.assertItemHasType(TEST_ITEM, TEST_ITEM_CATEGORY_OTHER);
+	}
+
+	@Category({On.Category.class})
+	@Test public void testShowKeywords() {
+		KeywordsDialogActor keywords = itemEdit.help().showKeywords();
+		keywords.assertKeywords(not(emptyString()));
+		keywords.close();
 	}
 }

android/src/main/java/net/twisterrob/inventory/android/content/model/CategoryVisuals.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 import javax.inject.Inject;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources.NotFoundException;
 import android.text.SpannableStringBuilder;
@@ -95,8 +96,8 @@ public class CategoryVisuals {
 		}
 	}
 
-	public void showKeywords(long categoryID) {
+	public void showKeywords(@NonNull Activity activity, long categoryID) {
 		CharSequence keywords = getKeywords(cache.getCategoryKey(categoryID), true);
-		ChangeTypeDialog.showKeywords(context, cache.getCategoryPath(categoryID), keywords);
+		ChangeTypeDialog.showKeywords(activity, cache.getCategoryPath(categoryID), keywords);
 	}
 }

android/src/main/java/net/twisterrob/inventory/android/fragment/data/BaseEditFragment.java
@@ -236,7 +236,7 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 					Hinter.unhighlight(name.getText());
 				}
 				@Override public void categoryQueried(long categoryID) {
-					visuals.showKeywords(categoryID);
+					visuals.showKeywords(requireActivity(), categoryID);
 				}
 			});
 			hint.setAdapter(hinter.getAdapter());
@@ -281,7 +281,7 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 		// CONSIDER setOnItemLongClickListener is not supported, any way to work around? So user has the same "tooltip" as in ChangeTypeDialog
 		type.setOnLongClickListener(new OnLongClickListener() {
 			@Override public boolean onLongClick(View view) {
-				visuals.showKeywords(getTypeId());
+				visuals.showKeywords(requireActivity(), getTypeId());
 				return true;
 			}
 		});
@@ -330,7 +330,7 @@ public abstract class BaseEditFragment<T, DTO extends ImagedDTO> extends BaseSin
 			updateHint(name.getText(), true);
 			return true;
 		} else if (id == R.id.action_category_keywords) {
-			visuals.showKeywords(getTypeId());
+			visuals.showKeywords(requireActivity(), getTypeId());
 			return true;
 		} else {
 			return super.onContextItemSelected(item);

android/src/androidTest/java/net/twisterrob/inventory/android/test/actors/CategoryActivityActor.java
@@ -1,0 +1,21 @@
+package net.twisterrob.inventory.android.test.actors;
+
+import net.twisterrob.inventory.android.R;
+import net.twisterrob.inventory.android.activity.data.CategoryActivity;
+
+public class CategoryActivityActor extends ItemContainingViewActivityActor {
+	public CategoryActivityActor() {
+		super(CategoryActivity.class);
+	}
+
+	@Override public void assertShowing(String itemName) {
+		assertActionTitle(itemName);
+	}
+
+	public CategoryActivityActor flatten() {
+		clickActionBar(R.id.action_category_viewAllItems);
+		CategoryActivityActor activity = new CategoryActivityActor();
+		activity.assertIsInFront();
+		return activity;
+	}
+}

android/src/androidTest/java/net/twisterrob/inventory/android/test/actors/EditActivityActor.java
@@ -99,6 +99,12 @@ public abstract class EditActivityActor extends ActivityActor {
 	public void checkPicture(@ColorInt int backgroundColor) {
 		onView(imageMatcher).perform(scrollTo()).check(matches(withBitmap(hasBackgroundColor(backgroundColor))));
 	}
+	public InfoPopupActor help() {
+		onView(withId(R.id.help)).perform(click());
+		InfoPopupActor popup = new InfoPopupActor();
+		popup.assertDisplayed();
+		return popup;
+	}
 	public SaveResultActor save() {
 		onView(withId(R.id.btn_save)).perform(click());
 		return new SaveResultActor();
@@ -159,6 +165,30 @@ public abstract class EditActivityActor extends ActivityActor {
 		public final void checkToastAlreadyExists() {
 			Matcher<String> matcher = containsStringRes(R.string.generic_error_unique_name);
 			assertToastMessage(withText(matcher));
+		}
+	}
+
+	public static class InfoPopupActor {
+		public void assertDisplayed() {
+			onRoot(isPlatformPopup()).check(matches(isCompletelyDisplayed()));
+		}
+
+		public KeywordsDialogActor showKeywords() {
+			onData(withMenuItemId(R.id.action_category_keywords)).perform(click());
+			KeywordsDialogActor dialog = new KeywordsDialogActor();
+			dialog.assertDisplayed();
+			return dialog;
+		}
+
+		public CategoryActivityActor jumpToCategory() {
+			onData(withMenuItemId(R.id.action_category_goto)).perform(click());
+			CategoryActivityActor activity = new CategoryActivityActor();
+			activity.assertIsInFront();
+			return activity;
+		}
+
+		public void suggestCategories() {
+			onData(withMenuItemId(R.id.action_category_suggest)).perform(click());
 		}
 	}
 }

android/src/androidTest/java/net/twisterrob/inventory/android/test/actors/KeywordsDialogActor.java
@@ -1,9 +1,12 @@
 package net.twisterrob.inventory.android.test.actors;
 
+import org.hamcrest.Matcher;
+
 import static org.hamcrest.Matchers.anyOf;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.test.espresso.Espresso;
 
@@ -27,6 +30,9 @@ public class KeywordsDialogActor extends AlertDialogActor {
 
 	public void assertKeywords(@StringRes int keywords) {
 		assertDialogMessage(withText(keywords));
+	}
+	public void assertKeywords(@NonNull Matcher<String> matcher) {
+		assertDialogMessage(withText(matcher));
 	}
 
 	public void close() {