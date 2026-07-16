0f3b70a0fd45dcca813b6199906537924dbfc0a5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-07-16 22:59:09 +0100

    Replace deprecated Orbit APIs

--- android/space/src/main/kotlin/net/twisterrob/inventory/android/viewmodel/OrbitViewModel.kt
+++ android/space/src/main/kotlin/net/twisterrob/inventory/android/viewmodel/OrbitViewModel.kt
@@ -5,13 +5,13 @@ import net.twisterrob.inventory.android.arch.UiState
 import net.twisterrob.inventory.android.content.VariantViewModel
 import net.twisterrob.inventory.android.logger
 import net.twisterrob.orbit.logging.decorateLogging
-import org.orbitmvi.orbit.ContainerHost
-import org.orbitmvi.orbit.viewmodel.container
+import org.orbitmvi.orbit.OrbitContainerHost
+import org.orbitmvi.orbit.viewmodel.orbitContainer
 
 abstract class OrbitViewModel<State : UiState, Effect : UiEffect>(
 	initialState: State
-) : VariantViewModel(), ContainerHost<State, Effect> {
+) : VariantViewModel(), OrbitContainerHost<State, State, Effect> {
 
-	override val container = container<State, Effect>(initialState)
+	override val container = orbitContainer<State, Effect>(initialState)
 		.decorateLogging(logger(this::class))
 }
