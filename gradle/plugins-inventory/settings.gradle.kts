d4e8066c000365d0ee8a292abb43f33067b0df8e
Róbert Papp <papp.robert.s@gmail.com>
2026-09-23 00:55:09 +0100

    Update nagging plugin version to 0.20

--- gradle/plugins-inventory/settings.gradle.kts
+++ gradle/plugins-inventory/settings.gradle.kts
@@ -1,7 +1,7 @@
 import net.twisterrob.gradle.doNotNagAbout
 
 plugins {
-	id("net.twisterrob.gradle.plugin.nagging") version "0.19"
+	id("net.twisterrob.gradle.plugin.nagging") version "0.20"
 }
 
 dependencyResolutionManagement {
