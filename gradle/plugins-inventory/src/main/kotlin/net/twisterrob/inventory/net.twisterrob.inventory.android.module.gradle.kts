90acfcdd077c8829de7ca005edb8619806526a8f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-09-23 00:09:15 +0100

    centralize

--- gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.android.module.gradle.kts
+++ gradle/plugins-inventory/src/main/kotlin/net/twisterrob/inventory/net.twisterrob.inventory.android.module.gradle.kts
@@ -18,3 +18,13 @@ afterEvaluate {
 }
 
 android.buildFeatures.buildConfig = false
+androidComponents {
+	finalizeDsl {
+		if (android.buildFeatures.buildConfig!!) {
+			tasks.withType<JavaCompile>().configureEach {
+				// Generated BuildConfig.java starts with `/** */ package` which is invalid.
+				options.compilerArgs.add("-Xlint:-dangling-doc-comments")
+			}
+		}
+	}
+}
